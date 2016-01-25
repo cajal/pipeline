5b6e88063391b9c2746f4571cfd917d6d8916a6a
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-01-25 02:35:19 -0600

    added matlab/getLocalPath.m; fixed rf.FOV foreign key

--- matlab/schemas/+pre/getSchema.m
+++ matlab/schemas/+pre/getSchema.m
@@ -1,7 +1,7 @@
 function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'pre', 'pipeline_pre');
+    schemaObject = dj.Schema(dj.conn, 'pre', 'pipeline_preprocessing');
 end
 obj = schemaObject;
 end
--- matlab/schemas/+rf/FOV.m
+++ matlab/schemas/+rf/FOV.m
@@ -1,13 +1,13 @@
 %{
-rf.FOV (manual) # field of view sizes for lenses
-setup :  tinyint  # two-photon setup number 
+rf.FOV (lookup) # my newest table
 -> rf.Lens
-take : tinyint  # measurement number 
-mag  : decimal(5,2)  # magnification
------
-width  : decimal(5,1)  # (um) horizontal FOV size 
-height : decimal(5,1)  # (um) vertical FOV size
-time = CURRENT_TIMESTAMP :timestamp
+mag             : decimal(5,2)           # ScanImage zoom factor
+setup           : tinyint                # 
+take            : tinyint                # 
+---
+height                      : decimal(5,1)                  # measured width of field of view along axis of pipette (medial/lateral on mouse)
+width                       : decimal(5,1)                  # measured width of field of view perpendicular to pipette (rostral/caudal on mouse)
+fov_date="2015-01-01"       : date                          # fov measurement date
 %}
 
 classdef FOV < dj.Relvar
