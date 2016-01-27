d3d1b65884489c5c05b28a057daba1aa58133b64
Fabian Sinz <sinz@bcm.edu>
2016-01-27 13:12:06 -0600

    adapted schema

--- python/pipeline/pre.py
+++ python/pipeline/pre.py
@@ -1,8 +1,9 @@
 import datajoint as dj
-from djaddon import gitlog
+from djaddon import gitlog, hdf5
 import numpy as np
 import c2s
 import matplotlib.pyplot as plt
+import seaborn as sns
 schema = dj.schema('pipeline_preprocessing', locals())
 
 
@@ -68,6 +69,7 @@ class NMFSegment(dj.Imported):
 
 
 @schema
+@hdf5
 class SegmentationTile(dj.Computed):
     definition = None
 
@@ -75,67 +77,86 @@ class SegmentationTile(dj.Computed):
         raise NotImplementedError('This table is populated from matlab.')
 
 
-@schema
-class SelectedMask(dj.Computed):
-    definition = None
+# @schema
+# class SelectedMask(dj.Computed):
+#     definition = None
 
-    def _make_tuples(self, key):
-        raise NotImplementedError('This table is populated from matlab.')
+#     def _make_tuples(self, key):
+#         raise NotImplementedError('This table is populated from matlab.')
 
-    def load_masks(self, key):
-        d1, d2 = (ScanInfo() & key).fetch1['px_height', 'px_width']
-        selection = (SegmentationTile() & self & key).project('mask')
-        masks = np.zeros((d1, d2, len(selection)))
-        for i, (i1, i2, j1, j2, mask) in enumerate(zip(*selection.fetch['rstart','rend','cstart','cend','mask'])):
-            masks[i1-1:i2,j1-1:j2,i]  = mask
-        return masks
+#     def load_masks(self, key):
+#         d1, d2 = (ScanInfo() & key).fetch1['px_height', 'px_width']
+#         selection = (SegmentationTile() & self & key).project('mask')
+#         masks = np.zeros((d1, d2, len(selection)))
+#         for i, (i1, i2, j1, j2, mask) in enumerate(zip(*selection.fetch['rstart','rend','cstart','cend','mask'])):
+#             masks[i1-1:i2,j1-1:j2,i]  = mask
+#         return masks
 
-    def plot_masks(self, key):
-        masks = self.load_masks(key) 
-        amask = (masks>0).sum(axis=2)
-        v = np.unique(amask)
-        masked = np.ma.masked_where(masks == 0, masks)
+#     def plot_masks(self, key, savedir='./'):
+#         if savedir[-1] != '/': savedir += '/'
         
-        for i in range(masks.shape[2]):
-            fig, ax = plt.subplots()
-            ax.contour(amask, v)
-            ax.imshow(masked[::-1,:,i], cmap=plt.cm.BuPu)
-            ax.set_aspect(1)
-            ax.axis('tight')
-            plt.show()
-            exit()
-@schema
-class MaskAverageTrace(dj.Computed):
-    definition = None
+#         masks = self.load_masks(key) 
+#         theCM = plt.cm.RdBu_r
+#         #theCM._init()
+#         #alphas = np.abs(np.linspace(0, 1.0, theCM.N))
+#         #theCM._lut[:-3,-1] = alphas
+        
+#         d1, d2, frames = masks.shape
+#         xaxis, yaxis = np.arange(d2), np.arange(d1)
+#         sns.set_style('white')
+#         for cell in range(frames):
+#             print('Plotting cell %i/%i' % (cell+1, frames))
+#             fig, ax = plt.subplots()
+#             for i in range(frames):
+#                 ma = masks[...,i]/masks[...,i].sum()
+#                 m = ma.ravel()[np.argsort(-ma.ravel())]
+#                 mc = np.cumsum(m)
+#                 th = m[mc > 0.9].max()
+#                 if cell == i:
+#                     cell_ma = ma < th
+#                 ax.contour(xaxis, yaxis, ma >= th, [.5], colors='silver')
 
-    def _make_tuples(self, key):
-        raise NotImplementedError('This table is populated from matlab.')
+#             m = np.ma.masked_where((masks[...,cell] == 0) | cell_ma, masks[...,cell])
+#             ax.imshow(m, cmap=theCM, zorder=10)
+#             ax.set_aspect(1)
+#             ax.axis('tight')
+#             ax.axis('off')
+#             fig.subplots_adjust(left=0,right=1, bottom=0, top=1)
+#             fig.savefig(savedir + 'cell%03i.png' % cell)
+#             plt.close(fig)
+
+# @schema
+# class MaskAverageTrace(dj.Computed):
+#     definition = None
+
+#     def _make_tuples(self, key):
+#         raise NotImplementedError('This table is populated from matlab.')
 
 @schema
 class Tesselation(dj.Manual):
     definition = None
 
-@schema
-@gitlog
-class STMSpikeRate(dj.Computed):
-    definition = """
-    # inferred spike rate trace
+# @schema
+# @gitlog
+# class STMSpikeRate(dj.Computed):
+#     definition = """
+#     # inferred spike rate trace
 
-    ->MaskAverageTrace
-    ---
-    rate          : longblob  # inferred spikes
-    """
+#     ->MaskAverageTrace
+#     ---
+#     rate          : longblob  # inferred spikes
+#     """
 
-    def _make_tuples(self, key):
+#     def _make_tuples(self, key):
 
-        data = (MaskAverageTrace() * ScanInfo() & key).project('fps').fetch1()
-        data['calcium'] = (MaskAverageTrace()& key).fetch1['trace'].squeeze()
-        data = c2s.preprocess([data], fps=data['fps'])
-        data[0]['calcium'] = np.atleast_2d(data[0]['calcium'])
-        data = c2s.predict(data)
-        data = data[0]
-        data['rate'] = data.pop('predictions').squeeze()
-        data.pop('calcium')
-        data.pop('fps')
-        self.insert1(data)
+#         data = (MaskAverageTrace() * ScanInfo() & key).project('fps').fetch1()
+#         data['calcium'] = (MaskAverageTrace()& key).fetch1['trace'].squeeze()
+#         data = c2s.preprocess([data], fps=data['fps'])
+#         data[0]['calcium'] = np.atleast_2d(data[0]['calcium'])
+#         data = c2s.predict(data)
+#         data = data[0]
+#         data['rate'] = data.pop('predictions').squeeze()
+#         data.pop('calcium')
+#         data.pop('fps')
+#         self.insert1(data)
 
