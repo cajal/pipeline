f40d0f785fe483f9e4f26ecc3e1c83f19f36daec
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-02-13 18:44:18 -0600

    added pre.Slice to identify movie slices.

---
+++ matlab/schemas/+pre/AverageFrame.m
@@ -0,0 +1,22 @@
+%{
+pre.AverageFrame (imported) # my newest table
+# add primary key here
+-----
+# add additional attributes
+%}
+
+classdef AverageFrame < dj.Relvar & dj.AutoPopulate
+
+	properties
+		popRel  % !!! update the populate relation
+	end
+
+	methods(Access=protected)
+
+		function makeTuples(self, key)
+		%!!! compute missing fields for key here
+			self.insert(key)
+		end
+	end
+
+end
--- matlab/schemas/+pre/Template.m
+++ matlab/schemas/+pre/Template.m
@@ -1,6 +1,8 @@
 %{
 pre.Template (imported) # template for motion correction
--> pre.ScanCheck
+-> pre.ScanInfo
+-> pre.Slice
+channel : tinyint   # channel number
 -----
 template :longblob   # template image
 %}
@@ -8,21 +10,22 @@ template :longblob   # template image
 classdef Template < dj.Relvar & dj.AutoPopulate
     
     properties
-        popRel  = pre.ScanCheck
+        popRel  = pre.ScanInfo
     end
     
     methods(Access=protected)
         
         function makeTuples(self, key)
-            reader = rf.getReader(key);
-            nFrames = min(5000, reader.nFrames);
-            if nFrames == reader.nFrames
-                frames = 1:nFrames;
-            else
-                frames = randsample(1:reader.nFrames, nFrames)
-                
-                nFrames = min(5000, reader.nFrames);
-                self.insert(key)
+            reader = pre.getReader(key,'~/cache');
+            for slice = 1:reader.nslices
+                key.slice = slice;
+                for channel = 1:reader.nchannels
+                    key.channel = channel;
+                    frames = reader(:,:,channel,slice,round(linspace(1,reader.nframes,5000)));                    
+                    template = mean(frames,3);                    
+                    key.template = template;
+                    self.insert(key);
+                end
             end
         end
         
