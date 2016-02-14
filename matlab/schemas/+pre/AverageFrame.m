b40f6edb4f70aef46973466b6d20ef7b797fb382
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-02-13 22:27:04 -0600

    fixed bug in pre.AverageFrame

--- matlab/schemas/+pre/AverageFrame.m
+++ matlab/schemas/+pre/AverageFrame.m
@@ -19,19 +19,20 @@ classdef AverageFrame < dj.Relvar & dj.AutoPopulate
             tic
             fixRaster = get_fix_raster_fun(pre.AlignRaster & key);
             reader = pre.getReader(key);
+            nframes = reader.nframes;
             for islice = 1:reader.nslices
                 key.slice = islice;
                 fixMotion = get_fix_motion_fun(pre.AlignMotion & key);                
                 for ichannel = 1:reader.nchannels
                     key.channel = ichannel;
                     frame = 0;
-                    for iframe = 1:reader.nframes
+                    for iframe = 1:nframes
                         frame = frame + (fixMotion(fixRaster(reader(:,:,ichannel, islice, iframe)), iframe)).^q;
                         if ismember(iframe,[1 10 100 500 1000 5000 nframes]) || mod(iframe,10000)==0
                             fprintf('Frame %5d/%d  %4.1fs\n', iframe, nframes, toc);
                         end
                     end
-                    key.frame = (frame/reader.nframes).^(1/q);
+                    key.frame = (frame/nframes).^(1/q);
                     self.insert(key)
                 end
             end
