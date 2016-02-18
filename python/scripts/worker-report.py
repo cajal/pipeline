f1ef5439d9cf83c8a6b6b2bdaffac16fd411e034
Fabian Sinz <sinz@bcm.edu>
2016-02-18 12:36:58 -0600

    emulate Dimitri on slack

--- python/scripts/worker-report.py
+++ python/scripts/worker-report.py
@@ -5,7 +5,10 @@ Populate relations.
 Examples:
 	worker populate pipeline.pre.ExtractSpikes
 """
+import os
+import random
 from argparse import ArgumentParser
+from datetime import datetime
 from importlib import import_module
 from slacker import Slacker
 import time
@@ -14,30 +17,52 @@ import sys
 import pipeline
 import datajoint as dj
 
-def post_message(msg, token=None, channel='#bot_planet'):
-    """
-    Posts a message to the specified slack channel if toke is not None
-    :param msg: Message
-    :param token: Slack integration token
-    :param channel: slack channel (needs # in front)
-    """
+addr = ['Despicable masters', 'Evil overlords', 'Dear Villain Scientists', 'Ladies and Gentlemen',
+        'Fellow Slackers', '"Real" people', 'Dear Biomass', 'Datajoyous greetings']
+
+
+def post_report(comment, report, token=None, channel='#bot_planet'):
     if token is not None:
         slack = Slacker(token)
-        slack.chat.post_message(channel=channel, text=msg, as_user=True)
+        with open('report.txt', 'w') as fid:
+            fid.write(report)
+        slack.files.upload('report.txt', filetype='text', title='Minion report', initial_comment=comment,
+                           channels=channel)
+        # os.remove('./report.txt')
+
+
+def progress_bar(k, n, barlen=20):
+    p = 1 if n == 0 else (n - k) / n
+    eq = '=' * int(round(p * barlen))
+    spaces = ' ' * (barlen - len(eq))
+    return "[%s] %.1f%% (%i/%i)" % (eq + spaces, p * 100, n - k, n)
+
+
+def running_jobs(schema, module_name, rel_name):
+    running = schema.jobs & dict(table_name=module_name + '.' + rel_name)
+    error = len(running & 'status="error"')
+    running = len(running) - error
+    ret = "Minions: {0} working".format(running) + ", {0} in error".format(error) * (error > 0)
+    if len(ret) > 0:
+        ret = '(' + ret + ')'
+    return ret
+
 
 def generate_report(module_name):
     module = import_module('pipeline.' + module_name)
-    members = [m for m in dir(module) if not m.startswith('_')]
-    if 'Jobs' in members:
-        jobs = getattr(module, 'Jobs')
-        members.remove('Jobs')
-    for members in members:
-        klass = getattr(module, member)
-    # for member in inspect.getmembers(module):
-    #     if isinstance(member, (dj.Computed, dj.Imported)):
-    #         print(member)
-    #     elif isinstance(member, dj.schema):
-    #         schema = member
+
+    schema = inspect.getmembers(module, lambda x: isinstance(x, dj.schema))[0][1]
+
+    ret = []
+    for name, klass in inspect.getmembers(module,
+                                          lambda x: isinstance(x, type) and issubclass(x, (dj.Computed, dj.Imported))):
+        prog = klass().progress(display=False)
+
+        if prog[0] > 0:
+            line = '{:<28}  {:<43}  {:<30}'.format(module_name + '.' + name + ': ',
+                                                   progress_bar(*prog), running_jobs(schema, module_name, name))
+            ret.append(line)
+    return '\n'.join(ret)
 
 
 def main(argv):
@@ -48,12 +73,19 @@ def main(argv):
 
     args = parser.parse_args(argv[1:])
 
-    run_daemon = True # execute at least one loop
+    run_daemon = True  # execute at least one loop
 
     while run_daemon:
+        address = "{0}, here is the report from ".format(random.choice(addr)) + "*" + datetime.now().strftime(
+            "%A, %d. %B %Y %I:%M%p") + "*"
+        report = []
         for pipeline_name, module_names in pipeline._report_on.items():
-            for module_name in module_names:
-                generate_report(module_name)
+            report.append("_{0}_:".format(pipeline_name))
+            report.extend([generate_report(module_name) for module_name in module_names])
+            report.append('\n')
+
+        report = '\n'.join(report)
+        post_report(address, report, token=args.slack)
         run_daemon = args.daemon
     return 0
 
