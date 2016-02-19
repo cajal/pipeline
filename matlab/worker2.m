8a5a73d9d80125756c9e0fb6359e717669de525f
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-02-19 17:12:46 -0600

    corrected matlab workers

--- matlab/worker2.m
+++ matlab/worker2.m
@@ -1,6 +1,8 @@
-parpopulate(pre.ExtractSpikes)
-parpopulate(monet.DriftTrialSet)
-parpopulate(monet.DriftResponseSet)
-parpopulate(monet.VonMises)
-parpopulate(monet.RF)
-pause(1000)
+while true
+    parpopulate(pre.ExtractSpikes)
+    parpopulate(monet.DriftTrialSet)
+    parpopulate(monet.DriftResponseSet)
+    parpopulate(monet.VonMises)
+    parpopulate(monet.RF)
+    pause(1000)
+end
