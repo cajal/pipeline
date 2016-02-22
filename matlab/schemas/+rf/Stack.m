8164ef26fdb7a3682707a32a9cb9309910a9eeb0
jakereimer <jreimer@cns.bcm.edu>
2016-02-22 17:47:05 -0600

    MATLAB Stack and StackInfo classses
    Stack is manual and StackInfo is a skeleton that will be populated via
    Python to avoid de-interleaving issues with TIFFStack

---
+++ matlab/schemas/+rf/Stack.m
@@ -0,0 +1,17 @@
+%{
+rf.Stack (manual) # scanimage scan info
+-> rf.Session
+stack_idx        : smallint               # number of TIFF stack file
+---
+-> rf.Site
+bottom_z                    : int                           # z location at bottom of the stack
+surf_z                      : int                           # z location of surface
+laser_wavelength            : int                           # (nm)
+laser_power                 : int                           # (mW) to brain
+stack_notes                 : varchar(4095)                 # free-notes
+scan_ts=CURRENT_TIMESTAMP   : timestamp                     # don't edit
+%}
+
+classdef Stack < dj.Relvar
+    
+end
