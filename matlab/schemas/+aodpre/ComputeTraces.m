5426861723a1df3a23e815f5761eecec9fea2a58
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-02-22 14:38:17 -0600

    excluded the mean signal from PCA computation for AOD signal preprocessing

--- matlab/schemas/+aodpre/ComputeTraces.m
+++ matlab/schemas/+aodpre/ComputeTraces.m
@@ -23,9 +23,12 @@ classdef ComputeTraces < dj.Relvar & dj.AutoPopulate
                     [X, keys] = fetchn(aodpre.Timeseries*aodpre.PreprocessMethod & key & 'channel=1', 'trace');
                     keys = rmfield(keys, 'channel');
                     X = double([X{:}]);
-                    % subtract 1 principal component
+                    M = mean(X);
+                    % subtract 1 principal component  (not including means)
                     [U,D,V] = svds(X,1);
                     X = X - U*D*V';
+                    % add the mean back 
+                    X = bsxfun(@plus, X, M);
                     for i=1:length(keys)
                         tuple = keys(i);
                         tuple.trace = single(X(:,i));
