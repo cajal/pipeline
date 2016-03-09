cc3ddc9e37e9ab1c12b76c247e2477b423b14725
Fabian Sinz <fabee@epagoge.de>
2016-03-09 14:52:54 -0600

    Update worker1.m

--- matlab/worker1.m
+++ matlab/worker1.m
@@ -1,11 +1,11 @@
 r  = rf.Session & 'session_date>"2016-02"';
 while true
     parpopulate(pre.ScanInfo, r)
-    parpopulate(rf.Sync, r)
+    parpopulate(rf.Sync)
     parpopulate(pre.ScanCheck, r)
     parpopulate(pre.AlignRaster, r)
-    parpopulate(pre.AlignMotion)
-    parpopulate(pre.AverageFrame)
+    parpopulate(pre.AlignMotion, r)
+    parpopulate(pre.AverageFrame, r)
     parpopulate(pre.Segment)
     parpopulate(pre.ExtractTraces)
     pause(1000)
