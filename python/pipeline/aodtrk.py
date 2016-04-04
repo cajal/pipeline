f5e5f8b0f75250eb69c0a15daa47925dfe44fa93
jagrawal <jagrawal@bcm.edu>
2016-04-04 14:41:24 -0500

    added filters to reject frames

--- python/pipeline/aodtrk.py
+++ python/pipeline/aodtrk.py
@@ -239,3 +239,167 @@ class EyeFrame(dj.Computed):
         pupil_angle_std             : float                         # angle of major axis vs. horizontal axis in radians
         intensity_std               : float                         # standard deviation of the ROI pixel values
         """
+
+
+
+
+@schema
+class SelectionProtocol(dj.Lookup):
+    definition = """
+    # groups of filtering steps to reject bad frames
+
+    filter_protocol_id      : int   # id of the filtering protocol
+    ---
+    protocol_name           : char(50) # descriptive name of the protocol
+    """
+
+    contents = [
+        {'filter_protocol_id': 0, 'protocol_name': 'frame_intensity'},
+        {'filter_protocol_id': 1, 'protocol_name': 'int_and_ran_pupil_x_50_2'},
+        {'filter_protocol_id': 2, 'protocol_name': 'int_and_ran_pupil_x_75_2'},
+        {'filter_protocol_id': 3, 'protocol_name': 'int_and_ran_pupil_x_25_2'},
+        {'filter_protocol_id': 4, 'protocol_name': 'int_and_ran_pupil_pos'},
+        {'filter_protocol_id': 5, 'protocol_name': 'int_and_ran_pupil_pos_spikes_removed'},
+        {'filter_protocol_id': 6, 'protocol_name': 'int_and_ran_pupil_pos_spike_filter2'}
+    ]
+
+    def apply(self, frames, key):
+        print("Applying filter with protocol id :", key['filter_protocol_id'])
+        for step in (ProtocolStep() & key).fetch.order_by('priority').as_dict():
+            # embed()
+            print("....for protocol id:", key['filter_protocol_id'], "applying filter with filter_id = ",
+                  step['filter_id'])
+            frames = FrameSelector().apply(frames, step, param=step['filter_param'])
+        return frames
+
+
+@schema
+class FrameSelector(dj.Lookup):
+    definition = """
+    # single filters to reject frames
+    filter_id           : tinyint   # id of the filter
+    ---
+    filter_name         : char(50)   # descriptive name of the filter
+    """
+
+    contents = [
+        {'filter_id': 0, 'filter_name': 'intensity_filter'},
+        {'filter_id': 1, 'filter_name': 'ran_pupil_x_th'},
+        {'filter_id': 2, 'filter_name': 'ran_pupil_pos'},
+        {'filter_id': 3, 'filter_name': 'spike_filter'},
+        {'filter_id': 4, 'filter_name': 'spike_filter2'}
+    ]
+
+    def apply(self, frames, key, param):
+        """
+        Apply takes a restriction of EyeFrame.Detection() and returns an even more restricted set of frames
+
+        :param frames: restriction of EyeFrame.Detection()
+        :param key: key that singles out a single filter
+        :param param: parameters to the filter
+        :return: an even more restricted set of frames
+        """
+        which = (self & key).fetch1['filter_name']
+
+        if which == 'intensity_filter':
+            i = frames.fetch['intensity_std']
+            th = np.percentile(i, param[0]) / param[1]
+            return frames & 'intensity_std>{threshold}'.format(threshold=th)
+
+        if which == 'ran_pupil_x_th':
+            i = frames.fetch['pupil_x_std']
+            th = np.percentile(i, param[0])
+            return frames & 'pupil_x_std<{threshold}*{param}'.format(threshold=th, param=param[1])
+
+        if which == 'ran_pupil_pos':
+            i = frames.fetch['pupil_x_std']
+            j = frames.fetch['pupil_y_std']
+            pos = i*i + j*j
+            th = np.percentile(pos, param[0])
+            return frames & '(pupil_x_std*pupil_x_std + pupil_y_std*pupil_y_std)<{threshold}*{param}'.format(threshold=th, param=param[1])
+
+        if which == 'spike_filter':
+            ra = frames.fetch.order_by('frame')['pupil_r_minor']
+            fr = frames.fetch.order_by('frame')['frame']
+            slope_coll = []
+            for i in range(1,ra.size):
+                slope_coll.append((ra[i] - ra[i-1])/ (fr[i] - fr[i-1]))
+            slope_coll1 = abs(np.asarray(slope_coll))
+            frames_rej = [dict(frame=k) for k in fr[np.where(slope_coll1 > param)]]
+            return frames - frames_rej
+
+        if which == 'spike_filter2':
+            ra = frames.fetch.order_by('frame')['pupil_r_minor']
+            fr = frames.fetch.order_by('frame')['frame']
+            fr_rej=[]
+            for i in range(2, ra.size-2):
+                avg = (ra[i-2] + ra[i-1] + ra[i+1] + ra[i+2]) / 4
+                if abs(ra[i] - avg) > param:
+                    fr_rej.append(fr[i])
+            frames_rej = [dict(frame=k) for k in fr_rej]
+            return frames - frames_rej
+
+
+@schema
+class ProtocolStep(dj.Lookup):
+    definition = """
+    # single filter in a protocol to accept frames
+    -> SelectionProtocol
+    -> FrameSelector
+    priority                : int   # priority of the filter step, the low the higher the priority
+    ---
+    filter_param=null       : longblob # parameters that are passed to the filter
+    """
+
+    # define the protocols. Each protocol has one id, but can have several filters
+    contents = [  # parameter needs to be an array
+        # protocol 0 contains only one filter and is based on intensity
+        {'filter_protocol_id': 0, 'filter_id': 0, 'priority': 50, 'filter_param': np.array([50,2])},
+        # protocol 1 = intensity filter + ransac(50,2)
+        {'filter_protocol_id': 1, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 1, 'filter_id': 1, 'priority': 40, 'filter_param': np.array([50,2])},
+        # protocol 2 = intensity filter + ransac(75,2)
+        {'filter_protocol_id': 2, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 2, 'filter_id': 1, 'priority': 40, 'filter_param': np.array([75,2])},
+        # protocol 3 = intensity filter + ransac(25,2)
+        {'filter_protocol_id': 3, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 3, 'filter_id': 1, 'priority': 40, 'filter_param': np.array([25,2])},
+        # protocol 4 = intensity filter + ransac x2+y2
+        {'filter_protocol_id': 4, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 4, 'filter_id': 2, 'priority': 40, 'filter_param': np.array([97,2])},
+        # protocol 5 = intensity filter + ransac x2+y2 + spike filter
+        {'filter_protocol_id': 5, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 5, 'filter_id': 2, 'priority': 40, 'filter_param': np.array([97,2])},
+        {'filter_protocol_id': 5, 'filter_id': 3, 'priority': 50, 'filter_param': np.array(50)},
+        # protocol 6 = intensity filter + ransac x2+y2 + spike filter2
+        {'filter_protocol_id': 6, 'filter_id': 0, 'priority': 10, 'filter_param': np.array([50,2])},
+        {'filter_protocol_id': 6, 'filter_id': 2, 'priority': 40, 'filter_param': np.array([97,2])},
+        {'filter_protocol_id': 6, 'filter_id': 4, 'priority': 50, 'filter_param': np.array(35)}
+    ]
+
+
+@schema
+class SelectedFrame(dj.Computed):
+    definition = """
+    # This schema only contains detected frames that meet a particular quality criterion
+    -> EyeFrame.Detection
+    -> SelectionProtocol
+    ---
+    """
+
+    @property
+    def populated_from(self):
+        return TrackInfo() * SelectionProtocol() & EyeFrame()
+
+    def _make_tuples(self, key):
+        print("Key = ", key)
+        # embed()
+        frames = EyeFrame.Detection() & key
+        print('\tLength before filtering: {l}'.format(l=len(frames)))
+        # & key can be removed from the line below
+        frames = (SelectionProtocol() & key).apply(frames, key)
+        print('\tLength after filtering: {l}'.format(l=len(frames)))
+
+        for frame_key in frames.project().fetch.as_dict:
+            key.update(frame_key)
+            self.insert1(key)
