60ce4e38271b51d4be711b69859889bd2aa69664
olakiril <olakiril@hotmail.com>
2016-06-11 13:47:46 -0500

    monet.RF - hack for unequal times & traces
    difference by one is manually fixed

--- matlab/schemas/+monet/RF.m
+++ matlab/schemas/+monet/RF.m
@@ -57,6 +57,12 @@ classdef RF < dj.Relvar & dj.AutoPopulate
             caTimes = caTimes(key.slice:nslices:end);
             [X, traceKeys] = fetchn(pre.Spikes & key, 'spike_trace');
             X = [X{:}];
+            lenDif = length(caTimes)-size(X,1);
+            assert(lenDif>1 | lenDif<0,'Unequal traces & times!')
+            if lenDif==1 % don't know why this happens - needs fixing!
+                warning('Unequal vectors, equilizing caTimes...')
+                caTimes = caTimes(1:end-1);
+            end
             X = @(t) interp1(caTimes-caTimes(1), X, t, 'linear', nan);  % traces indexed by time
             ntraces = length(traceKeys);
             
