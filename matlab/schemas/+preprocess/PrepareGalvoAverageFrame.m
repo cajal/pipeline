e923fc29eb6a60b9ae2a7b1db866ea6de2e8f460
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-07-02 02:11:04 -0500

    bugfix in PrepareGalvoAverageFrame

--- matlab/schemas/+preprocess/PrepareGalvoAverageFrame.m
+++ matlab/schemas/+preprocess/PrepareGalvoAverageFrame.m
@@ -22,12 +22,12 @@ classdef PrepareGalvoAverageFrame < dj.Relvar
                 for ichannel = 1:nchannels
                     tic
                     key.channel = ichannel;
-                    print('Averaging slice %d/%d channel %d\n', islice, nslices, ichannel) 
+                    fprintf('Averaging slice %d/%d channel %d\n', islice, nslices, ichannel) 
                     tuple.channel = ichannel;
                     fixMotion = get_fix_motion_fun(preprocess.PrepareGalvoMotion & key);
                     frame = 0;
                     for iframe = 1:nframes
-                        frame = frame + (fixMotion(fixRaster(reader(:,:,ichannel, islice, iframe)), iframe)).^q;
+                        frame = frame + max(0,fixMotion(fixRaster(reader(:,:,ichannel, islice, iframe)), iframe)).^q;
                         if ismember(iframe,[1 10 100 500 1000 5000 nframes]) || mod(iframe,10000)==0
                             fprintf('Frame %5d/%d  %4.1fs\n', iframe, nframes, toc);
                         end
