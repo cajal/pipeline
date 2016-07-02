c2519da469f6a781b9fb42a8478a3a0428cc4a26
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-07-02 12:32:46 -0500

    added image save method to preprocess.PrepareGalvoAverageFrame.

--- matlab/schemas/+preprocess/PrepareGalvoAverageFrame.m
+++ matlab/schemas/+preprocess/PrepareGalvoAverageFrame.m
@@ -9,6 +9,20 @@ frame                       : longblob                      # average frame ater
 
 classdef PrepareGalvoAverageFrame < dj.Relvar
 	methods
+        
+        function save(self)
+            for key = fetch(preprocess.PrepareGalvoMotion & self)'
+                frames = fetchn(preprocess.PrepareGalvoAverageFrame & key, 'frame', 'ORDER BY frame DESC');
+                path = sprintf('~/Desktop/frames/frame%05u-%05u-%u.png', ...
+                    key.animal_id, key.scan_idx, key.slice);
+                frames = cellfun(@(f) sqrt((f-min(f(:)))/(max(f(:))-min(f(:)))+0.01)-0.1, frames, 'uni', false);
+                frames = cat(3,frames{:});
+                if size(frames,3) == 2
+                    frames(:,:,3)=0;
+                end
+                imwrite(frames, path)
+            end
+        end
 
 		function makeTuples(self, key, reader)
             q = 6;
