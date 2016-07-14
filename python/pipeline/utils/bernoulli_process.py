dffeb3fa65c7cb4092ad71cb95bdac9b8c5ca275
Wenbo Wu <ww23@rice.edu>
2016-07-14 14:49:04 -0500

    GPU with Stochastic gradient descent

--- python/pipeline/utils/bernoulli_process.py
+++ python/pipeline/utils/bernoulli_process.py
@@ -5,16 +5,37 @@ from scipy.optimize import minimize
 T = th.tensor
 import theano.tensor.nnet.conv3d2d
 from sklearn.metrics import roc_auc_score
+from theano import shared
 th.config.floatX = 'float32'
 floatX = th.config.floatX
 
 
-class BP:
-    def __init__(self, pixel):
+class BernoulliProcess:
+    def __init__(self, pixel, exponentials=4, linear_channels=4, quadratic_channels=4):
         if np.any(np.array(pixel) % 2 == 0):
             raise ValueError("Pixel size should be odd.")
         self.pixel = pixel
-        self.parameters = OrderedDict()
+
+        # self.parameters = OrderedDict()
+        assert linear_channels > 0, "should use at least one linear channel."
+        assert quadratic_channels > 0, "should use at least one quadratic channel."
+
+
+        self.linear_channels = linear_channels
+        self.quadratic_channels = quadratic_channels
+        self.common_channels = exponentials
+
+        flt_width, flt_height = self.pixel
+
+        # horizontal components of the filters
+        self.u = shared(value=np.random.rand(quadratic_channels, flt_width, flt_height).astype(floatX) / (quadratic_channels*flt_width*flt_height), name='u', borrow=True)
+        # horizontal components of the filters
+        self.w = shared(value=np.random.rand(linear_channels, flt_width, flt_height).astype(floatX) / (linear_channels*flt_width*flt_height), name='w', borrow=True)
+
+        self.beta = shared(value=np.random.randn(exponentials, quadratic_channels).astype(floatX), name='beta', borrow=True)
+        self.gamma = shared(value=np.random.randn(exponentials, linear_channels).astype(floatX), name='gamma', borrow=True)
+        self.b = shared(value=np.random.randn(exponentials).astype(floatX), name='b', borrow=True)
+
 
     def _build_label_stack(self, data_shape, cell_locations):
         """
@@ -26,30 +47,119 @@ class BP:
         :return: numpy array with stack indicating the cell locations.
         """
         y_shape = tuple(i - j + 1 for i, j in zip(data_shape, self.pixel))
-        Y = np.zeros(y_shape)
+        Y = np.zeros(y_shape, dtype=floatX)
         j, i = cell_locations.T
         Y[i, j] = 1
 
         return Y
 
-    def _single_cross_entropy(self, data_shape):
-        X_ = T.fmatrix('stack')  # th.shared(np.require(Y, dtype=floatX), borrow=True, name='cells')
-        Y_ = T.fmatrix('cells')  # th.shared(np.require(Y, dtype=floatX), borrow=True, name='cells')
-        p_, parameters_ = self._build_probability_map(X_, data_shape)
+    def _single_cross_entropy(self, data_shape, learning_rate):
+        X_ = T.tensor3('stack', dtype=floatX) # no images x 1 x row x col
+        Y_ = T.tensor3('cells', dtype=floatX) # no images x row x col
 
-        loglik_ = Y_ * T.log(p_) + (1 - Y_) * T.log(1 - p_)
+        in_channels = 1
+        batchsize, in_row, in_col = data_shape
+        flt_row, flt_col = self.pixel
+
+        quadratic_features_ = T.nnet.conv2d(
+            # expects (batch size, channels, row, col)
+            input=X_.dimshuffle(0,'x',1,2),
+            # expects nb filters, channels, nb row, nb col
+            filters=self.u.dimshuffle(0,'x',1,2),
+            filter_shape=(self.u.get_value(borrow=True).shape[0], in_channels, flt_row, flt_col),
+            input_shape=(batchsize, in_channels, in_row, in_col),
+            border_mode='valid',
+        )   #(15,3,256,256)
+
+        linear_features_ = T.nnet.conv2d(
+            # expects (batch size,  channels, row, col)
+            input=X_.dimshuffle(0,'x',1,2),
+            # expects nb filters, channels, nb row, nb col
+            filters=self.w.dimshuffle(0,'x',1,2),
+            filter_shape=(self.w.get_value(borrow=True).shape[0], in_channels, flt_row, flt_col),
+            input_shape=(batchsize, in_channels, in_row, in_col),
+            border_mode='valid',
+        )   #(15,3,256,256)
+
+        quadr_filter_ = T.tensordot(self.beta, quadratic_features_ ** 2, (1,1)) #4d (2,15,256,256)
+        lin_filter_ = T.tensordot(self.gamma, linear_features_, (1, 1)) #4d (2,15,256,256)
+
+        exponent_ = quadr_filter_ + lin_filter_ + self.b.dimshuffle(0, 'x', 'x','x')
+
+        p_ = T.exp(exponent_).sum(axis=0)
+        p2_ = p_ / (1 + p_) * (1 - 2 * 1e-8) + 1e-8  #3d (15,254,254)
+        loglik_ = Y_ * T.log(p2_) + (1 - Y_) * T.log(1 - p2_)
         cross_entropy_ = -T.mean(loglik_)
+        g_u_ = T.grad(cross_entropy_, self.u)
+        g_w_ = T.grad(cross_entropy_, self.w)
+        g_beta_ = T.grad(cross_entropy_, self.beta)
+        g_gamma_ = T.grad(cross_entropy_, self.gamma)
+        g_b_ = T.grad(cross_entropy_, self.b)
 
-        dcross_entropy_ = T.grad(cross_entropy_, parameters_)
+        updates = [(self.u, self.u - learning_rate * g_u_),
+                   (self.w, self.w - learning_rate * g_w_),
+                   (self.beta, self.beta - learning_rate * g_beta_),
+                   (self.gamma, self.gamma - learning_rate * g_gamma_),
+                   (self.b, self.b - learning_rate * g_b_)]
 
-        return th.function((X_, Y_) + parameters_, cross_entropy_, allow_input_downcast=True), \
-               th.function((X_, Y_) + parameters_, dcross_entropy_, allow_input_downcast=True)
+        train_model = th.function(inputs=[X_, Y_], outputs=cross_entropy_, updates=updates)
+
+        # f = th.function([X_], linear_features_, allow_input_downcast=True)
+        #
+        # import numpy as np
+        # X = np.random.randn(15,256,256).astype('float32')
+        # Y = np.random.randint(2, size=(15,254,254))
+        # fx = f(X)
+        # #----------------------
+        # # TODO: remove later
+        # from IPython import embed
+        # embed()
+        # #exit()
+        # #----------------------
+
+        return train_model
+
+    def _build_p_map(self, data_shape, X_):
+
+        in_channels = 1
+        batchsize, in_row, in_col = data_shape
+        flt_row, flt_col = self.pixel
+
+        quadratic_features_ = T.nnet.conv2d(
+            # expects (batch size, channels, row, col)
+            input=X_.dimshuffle(0, 'x', 1, 2),
+            # expects nb filters, channels, nb row, nb col
+            filters=self.u.dimshuffle(0, 'x', 1, 2),
+            filter_shape=(self.u.get_value(borrow=True).shape[0], in_channels, flt_row, flt_col),
+            input_shape=(batchsize, in_channels, in_row, in_col),
+            border_mode='valid',
+        )  # (15,3,256,256)
+
+        linear_features_ = T.nnet.conv2d(
+            # expects (batch size,  channels, row, col)
+            input=X_.dimshuffle(0, 'x', 1, 2),
+            # expects nb filters, channels, nb row, nb col
+            filters=self.w.dimshuffle(0, 'x', 1, 2),
+            filter_shape=(self.w.get_value(borrow=True).shape[0], in_channels, flt_row, flt_col),
+            input_shape=(batchsize, in_channels, in_row, in_col),
+            border_mode='valid',
+        )  # (15,3,256,256)
+
+        quadr_filter_ = T.tensordot(self.beta, quadratic_features_ ** 2, (1, 1))  # 4d (2,15,256,256)
+        lin_filter_ = T.tensordot(self.gamma, linear_features_, (1, 1))  # 4d (2,15,256,256)
+
+        exponent_ = quadr_filter_ + lin_filter_ + self.b.dimshuffle(0, 'x', 'x', 'x')
+
+        p_ = T.exp(exponent_).sum(axis=0)
+        p2_ = p_ / (1 + p_) * (1 - 2 * 1e-8) + 1e-8  # 3d (15,254,254)
+
+        return p2_
 
     def P(self, X):
-        X_ = T.fmatrix('stack')  # th.shared(np.require(Y, dtype=floatX), borrow=True, name='cells')
-        p_, params_ = self._build_probability_map(X_, X.shape)
-        p = th.function((X_,) + params_, p_, allow_input_downcast=True)
-        P = p(*((X,) + tuple(self.parameters.values())))
+        X_ = T.tensor3('stack_P', dtype=floatX)  # th.shared(np.require(Y, dtype=floatX), borrow=True, name='cells')
+        p_ = self._build_p_map(X.shape, X_)
+        p = th.function([X_], p_, allow_input_downcast=True)
+        P = p(X)
 
         return P
 
@@ -61,135 +171,44 @@ class BP:
         :param kwargs: additionaly keyword arguments passed to sklearn.metrics.roc_auc_score
         :return: area under the curve
         """
-        return roc_auc_score(self._build_label_stack(X.shape, cell_locations).ravel(), self.P(X).ravel(), **kwargs)
+        result_list = []
+        i = 0
+        p_array = self.P(X)
+        for p_map in p_array:
+            result_list.append(roc_auc_score(self._build_label_stack(X[i].shape, cell_locations[i]).ravel(), p_map.ravel(), **kwargs))
+            i += 1
+        return result_list
 
-    def fit(self, stacks, cell_locations, **options):
+    def fit(self, stacks, cell_locations, learning_rate, grad_stacks, grad_locations, **options):
         """
         Fits the model.
         :param stacks: Iterable of stacks. All of them need to have the same size.
         :param cell_locations: Iterable of cell locations (N X 3 array of dtype int)
         :param options: options for scipy.minimize
         """
-        ll, dll = self._single_cross_entropy(stacks[0].shape)
+        print("stack shape : {}".format(stacks.shape))
+        ll = self._single_cross_entropy(stacks.shape, learning_rate)
         Ys = [self._build_label_stack(x.shape, c) for x, c in zip(stacks, cell_locations)]
-        slices, shapes = [], []
-        i = 0
-        for elem in self.parameters.values():
-            slices.append(slice(i, i + elem.size))
-            shapes.append(elem.shape)
-            i += elem.size
-        iteration = 0
-        def ravel(params):
-            return np.hstack([e.ravel() for e in params])
+        Ys = np.asarray(Ys)
 
-        def unravel(x):
-            return tuple(x[sl].reshape(sh) for sl, sh in zip(slices, shapes))
+        pre_val_auc = 0
+        iteration = 1
+        # while True:
+        for i in range(10001):            # print("parameter beta : {}".format(self.parameters['beta'].get_value()))
+            # if iteration // 100 == 0:
+            #     val_auc = self.auc(grad_stacks, grad_locations)
+            #     print("validation auc result : {}".format(val_auc))
+            #     if pre_val_auc > val_auc:
+            #         break
+            #     else:
+            #         pre_val_auc = val_auc
 
-        def obj(par):
-            return np.mean([ll(*((x, y) + unravel(par))) for x, y in zip(stacks, Ys)])
-
-        def dobj(par):
-            return np.mean([ravel(dll(*((x, y) + unravel(par)))) for x, y in zip(stacks, Ys)], axis=0)
-
-        def callback(x):
-            nonlocal iteration
-            print('%03i Cross entropy:' % iteration, obj(x))
+            cross = ll(stacks, Ys)
+            print("parameter beta : {}".format(self.beta.get_value(borrow=True)))
+            print("{}: cross entropy {}".format(iteration, cross))
             iteration += 1
 
-        x0 = ravel(self.parameters.values())
 
-        # todo find a better way than to box constrain the parameters
-        opt_results = minimize(obj, x0, jac=dobj, method='L-BFGS-B', callback=callback,
-                               bounds=list(zip(-100 * np.ones(len(x0)), 100 * np.ones(len(x0)))),
-                               options={"maxiter": 200})
-        for k, param in zip(self.parameters, unravel(opt_results.x)):
-            self.parameters[k] = param
-
-
-class RDBP(BP):
-    def __init__(self, pixel, exponentials=2, linear_channels=2, quadratic_channels=2):
-        super(RDBP, self).__init__(pixel)
-        assert linear_channels > 0, "should use at least one linear channel."
-        assert quadratic_channels > 0, "should use at least one quadratic channel."
-        self.linear_channels = linear_channels
-        self.quadratic_channels = quadratic_channels
-        self.common_channels = exponentials
-        flt_width, flt_height = self.pixel
-
-        # horizontal components of the filters
-        self.parameters['u_xy'] = np.random.rand(quadratic_channels, flt_width, flt_height)
-        self.parameters['u_xy'] /= self.parameters['u_xy'].size
-
-        # horizontal components of the filters
-        self.parameters['w_xy'] = np.random.rand(linear_channels, flt_width, flt_height)
-        self.parameters['w_xy'] /= self.parameters['w_xy'].size
-
-        self.parameters['beta'] = np.random.randn(exponentials, quadratic_channels)  # Beta for quadratic
-        self.parameters['gamma'] = np.random.randn(exponentials, linear_channels)  # Gamma for linear
-        self.parameters['b'] = np.random.randn(exponentials)
-
-    def _build_separable_convolution(self, no_of_filters, X_, data_shape):
-        """
-        Builds a theano function that performas a 2d convolution which is separable in
-        xy vs. z on the stack.
-        :param no_of_filters: number of convolution filters
-        :param X_: 3d tensor representing the stack (row, col, depth (# of 2D images we want to process at once. ))
-        :param data_shape: shape of the real data (the tensor has no shape yet)
-        :return: theano symbolic expression, (Uxy tensor, Uz tensor)
-        """
-        Vxy_ = T.tensor3(dtype=floatX)
-        batchsize, in_channels = 1, 1
-        in_width, in_height = data_shape
-        flt_row, flt_col = self.pixel
-
-        xy_ = T.nnet.conv2d(
-            # expects (batch size, channels, row, col), transform in to (depth(# of 2D images), 1, row, col)
-            input=X_.dimshuffle('x', 'x', 0, 1),
-            # expects nb filters, channels, nb row, nb col
-            filters=Vxy_.dimshuffle(0, 'x', 1, 2),
-            filter_shape=(no_of_filters, in_channels, flt_row, flt_col),
-            input_shape=(batchsize, in_channels, in_width, in_height),
-            border_mode='valid'
-        ).dimshuffle(1, 2, 3)
-        return xy_, Vxy_
-
-    def _build_exponent(self, X_, data_shape):
-        """
-        Builds the exponent of the nonlinearty (see README or Theis et al. 2013)
-        :param X_: 3d tensor representing the stack (row, col, depth (# of 2D images we want to include in a stack. ))
-        :param data_shape: shape of the real data (the tensor has no shape yet)
-        :return: symbolic tensor for the exponent, (Uxy, Wxy, beta, gamma, b)
-        """
-
-        linear_channels, quadratic_channels, common_channels = \
-            self.linear_channels, self.quadratic_channels, self.common_channels
-
-        quadratic_filter_, Uxy_ = self._build_separable_convolution(quadratic_channels, X_, data_shape)
-        linear_filter_, Wxy_ = self._build_separable_convolution(linear_channels, X_, data_shape)
-        b_ = T.dvector()
-        beta_ = T.fmatrix()
-        gamma_ = T.fmatrix()
-
-        quadr_filter_ = T.tensordot(beta_, quadratic_filter_ ** 2, (1, 0))
-        lin_filter_ = T.tensordot(gamma_, linear_filter_, (1, 0))
-
-        exponent_ = quadr_filter_ + lin_filter_ + b_.dimshuffle(0, 'x','x')
-        return exponent_, (Uxy_, Wxy_, beta_, gamma_, b_)
-
-    def _build_probability_map(self, X_, data_shape):
-        """
-        Builds a theano symbolic expression that yields the estimated probability of a cell per voxel.
-        :param X_: 3d tensor representing the stack (row, col, depth(possible: # of 2D images in a stack))
-        :param data_shape: shape of the real data (the tensor has no shape yet)
-        :return: symbolic tensor for P, (Uxy, Uz, Wxy, Wz, beta, gamma, b)
-        """
-
-        exponent_, params_ = self._build_exponent(X_, data_shape)
-        p_ = T.exp(exponent_).sum(axis=0)
-
-        # apply logistic function to log p_ and add a bit of offset for numerical stability
-        p2_ = p_ / (1 + p_) * (1 - 2 * 1e-8) + 1e-8
-        return p2_, params_
 
     def __str__(self):
         return """
@@ -198,6 +217,7 @@ class RDBP(BP):
         linear components: %i
         common components: %i
         """ % (self.quadratic_channels, self.linear_channels, self.common_channels)
+
 
     def __repr__(self):
         return self.__str__()
