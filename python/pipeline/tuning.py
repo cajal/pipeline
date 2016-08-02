e67e3fcc05ea063325787b6c7f25da95a91de429
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-08-02 16:59:01 -0500

    added pipeline.tuning.DirectionalResponse in python

--- python/pipeline/tuning.py
+++ python/pipeline/tuning.py
@@ -3,6 +3,9 @@ Analysis of visual tuning: receptive fields, tuning curves, pixelwise maps
 """
 
 import numpy as np
+from scipy.interpolate import interp1d
+from scipy.signal import convolve
+
 import datajoint as dj
 from . import preprocess, vis   # needed for foreign keys
 
@@ -96,6 +99,7 @@ class MonetRF(dj.Computed):
     bin_size           : float                         # (ms) temporal bin size
     degrees_x          : float                         # degrees along x
     degrees_y          : float                         # degrees along y
+    stim_duration      : float                         # (s) total stimulus duration
     """
 
     key_source = preprocess.Spikes() * preprocess.Sync() & vis.Monet()
@@ -134,9 +138,6 @@ class MonetRF(dj.Computed):
             k = np.hamming(np.floor(half)*2+1)
             return k.reshape([1]*dim+[k.size])/k.sum()
 
-        from scipy.interpolate import interp1d
-        from scipy.signal import convolve
-
         # enter basic information about the RF Map
         nbins = 6
         bin_size = 0.1     # s
@@ -146,18 +147,19 @@ class MonetRF(dj.Computed):
         degrees_per_pixel = 180 / np.pi * diagonal * cm_per_inch / np.sqrt(np.float64(x)*x + np.float64(y)*y) / distance
         degrees_x = degrees_per_pixel * x
         degrees_y = degrees_per_pixel * y
-        self.insert1(dict(key, degrees_x=degrees_x, degrees_y=degrees_y, nbins=nbins, bin_size=bin_size*1000))
 
         # fetch traces and their slices (for galvo scans)
         trace_time = (preprocess.Sync() & key).fetch1['frame_times'].squeeze()  # calcium scan frame times
-        trace_list, slices, trace_keys = (
+        traces, slices, trace_keys = (
             preprocess.Spikes.RateTrace() * preprocess.ExtractRaw.GalvoROI() &
             key).fetch['rate_trace', 'slice', dj.key]
         n_slices = (preprocess.Prepare.Galvo() & key).fetch1['nslices']
-        assert n_slices*trace_list[0].size == trace_time.size, 'trace times must be a multiple of n_slices'
+        trace_time = trace_time[:n_slices*traces[0].size]  # truncate if interrupted scan
+        assert n_slices*traces[0].size == trace_time.size, 'trace times must be a multiple of n_slices'
         dt = (trace_time[1:]-trace_time[:-1]).mean()*n_slices
-        maps = [0]*len(trace_list)
+        maps = [0]*len(traces)
         n_trials = 0
+        stim_duration = 0.0
         for trial_key in (preprocess.Sync() * vis.Trial() * vis.Condition() &
                           'trial_idx between first_trial and last_trial' &
                           vis.Monet() & key).fetch.order_by('trial_idx').keys():
@@ -166,15 +168,16 @@ class MonetRF(dj.Computed):
 
             # get the movie and scale to [-1, +1]
             movie_times = (vis.Trial() & trial_key).fetch1['flip_times'].flatten()
+            stim_duration += movie_times[-1] - movie_times[0]
             movie = (vis.Monet() * vis.MonetLookup() & trial_key).fetch1['cached_movie']
             movie = (np.float32(movie) - 127.5) / 126.5
 
-            # rebin the movie to bin_size
+            # rebin the movie to bin_size.  Reverse time for convoluion.
             fps = 1 / (movie_times[1:] - movie_times[:-1]).mean()
             start_time = movie_times[0] + bin_size / 2
             movie = interp1d(
                 movie_times, convolve(
-                    movie, hamming(bin_size*fps, 2), 'same'))(np.r_[start_time:movie_times[-1]:bin_size])
+                    movie, hamming(bin_size*fps, 2), 'same'))(np.r_[movie_times[-1]:start_time:-bin_size])
 
             # compute the maps for each slice separately to account for time differences between slices
             for islice in set(slices):
@@ -184,7 +187,7 @@ class MonetRF(dj.Computed):
 
                 # rebin traces to bin_size
                 snippets = interp1d(time, convolve(
-                    np.stack(trace_list[ix]),
+                    np.stack(traces[ix].flatten()),
                     hamming(bin_size/dt, 1), 'same'), fill_value=0)(
                     np.r_[start_time+bin_size*(nbins-1):movie_times[-1]:bin_size])
 
@@ -192,9 +195,17 @@ class MonetRF(dj.Computed):
                 for i, snippet in zip(ix, snippets):
                     maps[i] += convolve(
                         movie,
-                        snippet[::-1].reshape((1, 1, snippet.size))/np.linalg.norm(snippet),
+                        snippet.reshape((1, 1, snippet.size))/np.linalg.norm(snippet),
                         mode='valid')
             print()
+
+        # submit data
+        self.insert1(dict(key,
+                          degrees_x=degrees_x,
+                          degrees_y=degrees_y,
+                          nbins=nbins,
+                          bin_size=bin_size * 1000,
+                          stim_duration=stim_duration))
         MonetRF.Map().insert(
             (dict(trace_key, map=m/n_trials) for trace_key, m in zip(trace_keys, maps)),
             ignore_extra_fields=True)
@@ -203,15 +214,15 @@ class MonetRF(dj.Computed):
 
 @schema
 class DirectionalResponse(dj.Computed):
-    definition = """
+    definition = """  # response to directional stimulus
     -> Directional
     -> preprocess.Spikes
     ---
-    latency : float   # latency used (s)
+    latency : float   # latency used (ms)
     """
 
-    class Trial(dj.Part):
-        definition = """
+    class TraceTrial(dj.Part):
+        definition = """   # the response for each trial and each trace
         -> DirectionalResponse
         -> preprocess.Spikes.RateTrace
         -> Directional.Trial
@@ -220,8 +231,37 @@ class DirectionalResponse(dj.Computed):
         """
 
     def _make_tuples(self, key):
-        # fetch traces
-        pass
+        print('Directional response for ', key)
+        traces, slices, trace_keys = (
+            preprocess.Spikes.RateTrace() * preprocess.ExtractRaw.GalvoROI() &
+            key).fetch['rate_trace', 'slice', dj.key]
+        traces = np.float64(np.stack(t.flatten() for t in traces))
+
+        #  fetch and clean up the trace time
+        trace_time = (preprocess.Sync() & key).fetch1['frame_times'].squeeze()  # calcium scan frame times
+        n_slices = (preprocess.Prepare.Galvo() & key).fetch1['nslices']
+        trace_time = trace_time[:n_slices*traces.shape[1]]  # truncate if interrupted scan
+        assert n_slices*traces[0].size == trace_time.size, 'trace times must be a multiple of n_slices'
+        trace_time = trace_time[::n_slices]    # keep trace times for the first slice only
+        dt = (trace_time[1:]-trace_time[:-1]).mean()
+
+        # compute and interpolate cumulative traces on time of first slice
+        assert traces.ndim == 2 and traces.shape[0] == len(trace_keys), 'incorrect trace dimensions'
+        traces = interp1d(trace_time, np.cumsum(traces, axis=1)*dt)
+
+        # insert responses for each trace and trial with time adjustment for slices
+        latency = 0.01  # s
+        self.insert1(dict(key, latency=1000*latency))
+        table = DirectionalResponse.TraceTrial()
+        for onset, offset, trial_key in zip(*(Directional.Trial() & key).fetch['onset', 'offset', dj.key]):
+            for islice in set(slices):
+                ix = np.where(slices == islice)[0]
+                responses = (traces(offset+latency-dt*islice) -
+                             traces(onset+latency-dt*islice))[ix]/(offset-onset)
+                table.insert((dict(trial_key, response=response, **trace_keys[i])
+                              for i, response in zip(ix, responses)),
+                             ignore_extra_fields=True)
+        print('Done')
 
 
 schema.spawn_missing_classes()
