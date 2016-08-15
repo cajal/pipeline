6111338cf17d1a9ad6547ec1858a45488ee368df
jakereimer <jreimer@cns.bcm.edu>
2016-08-15 16:45:01 -0500

    updated preprocess.Treadmill to use experiment.TreadmillSpecs

--- matlab/schemas/+preprocess/Treadmill.m
+++ matlab/schemas/+preprocess/Treadmill.m
@@ -2,10 +2,9 @@
 preprocess.Treadmill (imported) # 
 -> experiment.Scan
 ---
-treadmill_raw                        : longblob          # raw ball velocity
+treadmill_raw                        : longblob          # raw treadmill counts
 treadmill_vel                        : longblob          # ball velocity integrated over 100ms bins in cm/sec
-treadmill_time                      : longblob          # timestamps of each sample in seconds, with same t=0 as patch and camera data
-treadmill_cmsec_factor               : float             # factor used to convert to cm/2
+treadmill_time                       : longblob           # timestamps of each sample in seconds on behavior clock
 treadmill_ts=CURRENT_TIMESTAMP       : timestamp         # automatic
 %}
 
@@ -32,11 +31,18 @@ classdef Treadmill < dj.Relvar & dj.AutoPopulate
             
             tuple.treadmill_raw = unwrap(dat.ball(:,1),2^31);
             
+            % Calculate counts per bin
             bins100ms = ballT(1):.1:ballT(end);
-            distPerBin = [0 diff(interp1(ballT,tuple.treadmill_raw-tuple.treadmill_raw(1),bins100ms))]; % integrated distance per 100ms bin
+            countsPerBin = [0 diff(interp1(ballT,tuple.treadmill_raw-tuple.treadmill_raw(1),bins100ms))]; % integrated (+ and -) counts per 100ms bin
             
-            tuple.treadmill_cmsec_factor = .09; %(71.8168 cm circumference of 9inch wheel generates 8000 encoder counts = ~.009cm per count * 10 100ms bins per second)
-            tuple.treadmill_vel = interp1(bins100ms,distPerBin * tuple.treadmill_cmsec_factor,ballT);
+            % Get all prior treadmill spec keys
+            treadmillSpecKeys = fetch(experiment.TreadmillSpecs * experiment.Session & key & 'treadmill_start_date <= session_date','ORDER BY treadmill_start_date');
+            % Get diameter and encoder counts per revolution for most recent treadmill
+            [diam, counts] = fetch1(experiment.TreadmillSpecs & treadmillSpecKeys(end),'diameter','counts_per_revolution');
+            
+            cmPerCount = pi*diam/counts; % for example, 71.8168 cm circumference of 9inch wheel generates 8000 encoder counts = ~.009cm per count
+ 
+            tuple.treadmill_vel = interp1(bins100ms,countsPerBin * cmPerCount * 10,ballT); % multiply counts per bin * cm per count * ten 100ms bins per second to get cm/s
             tuple.treadmill_vel(isnan(tuple.treadmill_vel))=0;
            
             self.insert(tuple)
