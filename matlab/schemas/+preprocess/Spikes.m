19eaebdc541afda45620ec41cfc96127f05867ed
jakereimer <jreimer@cns.bcm.edu>
2016-08-31 17:10:58 -0500

    population of Spikes and ComputeTraces works correctly in MATLAB

--- matlab/schemas/+preprocess/Spikes.m
+++ matlab/schemas/+preprocess/Spikes.m
@@ -9,14 +9,23 @@ preprocess.Spikes (computed) # infer spikes from calcium traces
 classdef Spikes < dj.Relvar & dj.AutoPopulate
 
 	properties
-		popRel = preprocess.ComputeTraces*preprocess.SpikeMethod  % !!! update the populate relation
+        % NMF spikes (method 5) can be copied via MATLAB, STM spike detection is performed in Python
+		popRel = preprocess.ComputeTraces*preprocess.SpikeMethod  & preprocess.ExtractRawSpikeRate & 'spike_method=5';
 	end
 
 	methods(Access=protected)
 
 		function makeTuples(self, key)
-		%!!! compute missing fields for key here
+            
+            % Copy NMF spikes from preprocess.ExtractRawSpikeRate
+            tuples = fetch(preprocess.ExtractRawSpikeRate & key,'spike_trace->rate_trace','*');
+            tuples = rmfield(tuples,'channel');
+            [tuples.spike_method] = deal(5);
+            
 			self.insert(key)
+            insert(preprocess.SpikesRateTrace,tuples);
+            
+            
 		end
 	end
 
