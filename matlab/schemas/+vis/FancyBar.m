1bf0e4ae073f0c26fac4b9e9ff7289499ca477db
olakiril <olakiril@hotmail.com>
2016-09-08 12:00:20 -0500

    FancyBar

---
+++ matlab/schemas/+vis/FancyBar.m
@@ -0,0 +1,22 @@
+ %{
+vis.FancyBar (manual) # pink noise with periods of motion and orientation$
+-> vis.Condition
+---
+pre_blank                   : double                        # (s) blank period preceding trials
+luminance                   : float                         # (cd/m^2)
+contrast                    : float                         # michelson contrast
+speed                       : float                         # (degrees/s)
+frame_downsample            : tinyint                       # 1=60 fps, 2=30 fps, 3=20 fps, 4=15 fps, etc
+bar_width                   : float                         # Degrees
+grid_width                  : float                         # Degrees
+bar_speed                   : float                         # Bar speed in °/s
+flash_speed                 : float                         # cycles/sec temporal frequency of the grid flickering
+grating                     : float                         # selection beween grating (1) or flashing (0)
+grat_width                  : float                         # in cycles/deg
+grat_speed                  : float                         # in cycles/deg
+axis                        : float                         # the axis of the bar movement
+%}
+
+
+classdef FancyBar < dj.Relvar
+end
