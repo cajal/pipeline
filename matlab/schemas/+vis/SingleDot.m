0a3e639a3919aaa1c4ead5be2bdb249a0088a950
Shan Shen <shanshen@macbook-air.ad.bcm.edu>
2016-10-21 17:00:42 -0500

    Modify table SingleDot and remove DotMap

--- matlab/schemas/+vis/SingleDot.m
+++ matlab/schemas/+vis/SingleDot.m
@@ -2,16 +2,14 @@
 vis.SingleDot (manual) # single dot to map receptive field
 -> vis.Condition
 -----
-luminance                   : float                         # cd/m^2
-contrast                    : float                         # Michelson's 0-1
-bg_color                    : smallint                      # (0-255) the index of the background color
-dot_color                   : smallint                      # (0-255) the index of the dot color
+bg_level                    : smallint                      # (0-255) the index of the background luminance, 0 is black
+dot_level                   : smallint                      # (0-255) the index of the dot luminance, 0 is black
 dot_x                       : float                         # (fraction of the screen diagonal) position of dot on x axis
 dot_y                       : float                         # (fraction of the screen diagonal) position of dot on y axis
 dot_xsize                   : float                         # (fraction of the screen diagonal) width of dots
 dot_ysize                   : float                         # (fraction of the screen diagonal) height of dots
 dot_shape                   : enum('rect','oval')           # shape of the dot
-dot_time                    : smallint                      # time of each dot persists
+dot_time                    : float                         # (secs) time of each dot persists
 %}
 
 classdef SingleDot < dj.Relvar
