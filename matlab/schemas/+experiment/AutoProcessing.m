d9f2242d0d4f1ad3195be3e91482cc2d7676e6bb
Fabian Sinz <sinz@bcm.edu>
2017-01-30 17:25:42 -0600

    rename EnterProcessing to AutoProcessing

---
+++ matlab/schemas/+experiment/AutoProcessing.m
@@ -0,0 +1,9 @@
+%{
+experiment.AutoProcessing (manual) # scans that should enter automatic processing
+
+-> experiment.Scan
+-----
+%}
+
+classdef AutoProcessing < dj.Relvar
+end
--- matlab/schemas/+experiment/EnterProcessing.m
+++
@@ -1,9 +0,0 @@
-%{
-experiment.EnterProcessing (manual) # scans that should enter processing
-
--> experiment.Scan
------
-%}
-
-classdef EnterProcessing < dj.Relvar
-end
