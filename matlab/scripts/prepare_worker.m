f7b774000c9091e27127f157f3aac03354b1845d
Fabian Sinz <sinz@bcm.edu>
2017-02-04 13:15:25 -0600

    update prepare_worker for autoprocessing

--- matlab/scripts/prepare_worker.m
+++ matlab/scripts/prepare_worker.m
@@ -1,5 +1,11 @@
-r  = experiment.Session & 'session_date>"2016-02"';
+r  = experiment.Session & 'session_date>"2016-02"' & experiment.AutoProcessing;
+
 while true
-    parpopulate(preprocess.Prepare, r)
+    while count((preprocess.Prepare().popRel & r) - preprocess.Prepare)
+    	  parpopulate(preprocess.Prepare, r)
+    end
+    while count((preprocess.Sync().popRel & r) - preprocess.Sync)
+    	  parpopulate(preprocess.Sync, r)
+    end
     pause(1000)
 end
