6030ffc725c2859ea3318e60bae28986d3b17818
Fabian Sinz <sinz@bcm.edu>
2017-02-05 08:47:32 -0600

    update segmentation worker

--- matlab/scripts/segmentation_worker.m
+++ matlab/scripts/segmentation_worker.m
@@ -1,5 +1,8 @@
-r  = experiment.Session & 'session_date>"2016-02"';
+r  = experiment.Session & 'session_date>"2016-02"' & experiment.AutoProcessing;
+
 while true
-    parpopulate(preprocess.ExtractRaw, r)
+    while count((preprocess.ExtractRaw().popRel & r) - preprocess.ExtractRaw)
+    	  parpopulate(preprocess.ExtractRaw, r)
+    end
     pause(1000)
 end
