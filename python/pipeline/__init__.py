d7bda5f78f55143e0fca9dbe07f09175a9af3370
ecobost <ecobost@gmail.com>
2017-02-16 18:18:51 -0600

    Rewrote the PipelineException class to print the message sent (and the keys)

--- python/pipeline/__init__.py
+++ python/pipeline/__init__.py
@@ -6,16 +6,12 @@ _report_on = {
 }
 
 class PipelineException(Exception):
-    def __init__(self, message, keys=None):
-        # Call the base class constructor with the parameters it needs
-        super(Exception, self).__init__(message)
+    """Base pipeline exception. Prints the message plus any passed info."""
+    def __init__(self, message, info):
+        info_message = '\nError info: ' + repr(info) if info else ''
+        super().__init__(message + info_message)
+        self.info = info
 
-        self.keys = keys
-
-    def __str__(self):
-        return """
-        Pipeline Exception raised while processing {0}
-        """.format(repr(self.keys))
 
 # ----------- loads local configuration from file ----------------
 from .settings import Config, LOCALCONFIG, GLOBALCONFIG
