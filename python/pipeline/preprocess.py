8f96c4eba5d49179f6914c25d1a1392e4881fc65
ecobost <emcobost@gmail.com>
2017-02-27 13:13:37 -0600

    Add vmin, vmax to plots so we can compare among them

--- python/pipeline/preprocess.py
+++ python/pipeline/preprocess.py
@@ -194,10 +194,14 @@ class Prepare(dj.Imported):
         import matplotlib.animation as animation
 
         fig = plt.figure()
-        plt.subplot(1, 2, 1); plt.title('Original')
-        im1 = plt.imshow(original_scan[:, :, 0])  # just a placeholder
+        plt.subplot(1, 2, 1); plt.title('Original');
+        im1 = plt.imshow(original_scan[:, :, 0], vmin=original_scan.min(),
+                         vmax=original_scan.max())  # just a placeholder
+        plt.colorbar()
         plt.subplot(1, 2, 2); plt.title('Corrected')
-        im2 = plt.imshow(original_scan[:, :, 0])  # just a placeholder
+        im2 = plt.imshow(corrected_scan[:, :, 0], vmin=corrected_scan.min(),
+                         vmax=corrected_scan.max())  # just a placeholder
+        plt.colorbar()
 
         def update_img(i):
             im1.set_data(original_scan[:, :, i])
