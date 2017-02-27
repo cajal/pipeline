7c9eeda05f4d1e300a5655712a3b68a17e92af1c
Fabian Sinz <sinz@bcm.edu>
2017-02-27 07:40:48 -0700

    debug

--- python/pipeline/preprocess.py
+++ python/pipeline/preprocess.py
@@ -242,7 +242,7 @@ class CorrelationImage(dj.Computed):
 
         # Load the scan
         reader = TIFFReader(local_filename)
-        for sli, channel in  zip((Prepare.GalvoMotion() & key).fetch['slice', 'channel']):
+        for sli, channel in  zip(*(Prepare.GalvoMotion() & key).fetch['slice', 'channel']):
             xy_motion = (Prepare.GalvoMotion() & key & dict(slice=sli+1, channel=channel+1)).fetch1['motion_xy']
             print('Processing channel {} of slice {}'.format(channel+1, sli+1), flush=True)
             scan = np.double(reader[:, :, channel, sli, :]).squeeze()
