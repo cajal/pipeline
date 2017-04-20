00f7ab1ed474d986e68d13fc08b0317dbea554bc
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2017-04-19 21:10:30 -0500

    debugged and tested  stimulus.Matisse2, stimulus.Monet2 -- they are ready to be used.

--- matlab/schemas/+stimulus/+conf/matisse2.m
+++ matlab/schemas/+stimulus/+conf/matisse2.m
@@ -1,27 +1,27 @@
-function Matisse2
+function matisse2
 
 control = stimulus.getControl;
 control.clearAll   % clear trial queue and cached conditions.
 
 cond.fps = 60;
-cond.pre_blank_period = 0.1;
+cond.pre_blank_period = 0.5;
 cond.noise_seed = 100;
 cond.pattern_width = 64;
-cond.pattern_upscale = 3;
 cond.duration = 1;
 cond.pattern_aspect = 1.7;
-cond.ori = 0:90:359;
-cond.outer_ori_delta = 90;
-cond.coherence = [1 1.5 2.5];
-cond.aperture_x = -0.10;
-cond.aperture_y = +0.05;
+cond.ori = 0:45:359;
+cond.outer_ori_delta = -30:20:30;
+cond.coherence = 1.5;
+cond.aperture_x = -0.2:0.1:0.2;
+cond.aperture_y = -0.1:0.1:0.1;
 cond.aperture_r = 0.2;
 cond.aperture_transition = 0.1;
-cond.annulus_alpha = 0;
-cond.outer_contrast = [0.1 0.2 1];
-cond.inner_contrast = [0.1 0.2 1];
+cond.annulus_alpha = 0.0;
+cond.outer_contrast = 1;
+cond.inner_contrast = 1;
 cond.outer_speed = 0.2;
 cond.inner_speed = 0.2;
+
 
 % assert(isscalar(cond))
 params = stimulus.utils.factorize(cond);
--- matlab/schemas/+stimulus/Monet2.m
+++ matlab/schemas/+stimulus/Monet2.m
@@ -7,7 +7,7 @@ duration                    : decimal(6,3)                 # (s) trial duration
 rng_seed                    : double                       # random number generator seed
 pattern_width               : smallint                     # pixel size of the resulting pattern
 pattern_aspect              : float                        # the aspect ratio of the pattern
-pattern_upscale             : tinyint                      # integer upscale factor of the pattern
+-> stimulus.TempKernel
 temp_bandwidth              : decimal(4,2)                 # (Hz) temporal bandwidth of the stimulus
 ori_coherence               : decimal(4,2)                 # 1=unoriented noise. pi/ori_coherence = bandwidth of orientations.
 ori_fraction                : float                        # fraction of time coherent orientation is on
@@ -27,24 +27,27 @@ classdef Monet2 < dj.Manual & stimulus.core.Visual
     
     methods(Static)
         function test
-            cond.fps = 60;
+            cond.fps = 30;
             cond.duration = 30;
             cond.rng_seed = 1;
-            cond.pattern_width = 64;
+            cond.pattern_width = 80;
             cond.pattern_aspect = 1.7;
-            cond.pattern_upscale = 3;
-            cond.ori_coherence = 2.5;
+            cond.ori_coherence = 1.5;
             cond.ori_fraction = 0.4;
+            cond.temp_kernel = 'half-hamming';
             cond.temp_bandwidth = 4;
             cond.n_dirs = 16;
             cond.ori_mix = 1;
-            cond.speed = 0.5;
+            cond.speed = 0.25;
             
             tic
             cond = stimulus.Monet2.make(cond);
             toc
             
-            v = VideoWriter('Monet2', 'MPEG-4');
+            file = fullfile(pwd,'Monet2.mp4');
+            fprintf('Saving %s\n', file)
+            
+            v = VideoWriter(file, 'MPEG-4');
             v.FrameRate = cond.fps;
             v.Quality = 100;
             open(v)
@@ -55,36 +58,45 @@ classdef Monet2 < dj.Manual & stimulus.core.Visual
         function cond = make(cond)
             
             function y = hann(q)
-                % circuar hanning mask with symmetric opposite lobes
+                % hanning window
                 y = (0.5 + 0.5*cos(q)).*(abs(q)<pi);
             end
 
-            assert(~verLessThan('matlab','9.1'), 'Please upgrade MATLAB to R2016b or better')  % required for no bsxfun
+            assert(~verLessThan('matlab','9.1'), ...  % required for no bsxfun
+                'Please upgrade MATLAB to R2016b or better')  
             
-            period = cond.duration/cond.n_dirs;
-            nFrames = round(cond.duration*cond.fps/2)*2;
-            targetSize = [round(cond.pattern_width/cond.pattern_aspect/2)*2, cond.pattern_width, nFrames];
-            
-            assert(~any(bitand(targetSize,1)), 'all movie dimensions must be even')
-            r = RandStream.create('mt19937ar','NormalTransform', ...
-                'Ziggurat', 'Seed', cond.rng_seed);
-            m = r.randn(targetSize + cond.pattern_upscale*[2 2 0]);  % movie with padding to prevent edge correlations
             
             % apply temporal filter in time domain
-            semi = round(cond.fps/cond.temp_bandwidth);
-            k = hamming(semi*2+1);
-            k = k(1:semi+1);
-            k = k/sum(k);
-            m = convn(m, permute(k, [3 2 1]), 'same');
+            switch cond.temp_kernel
+                case 'hamming'
+                    semi = round(cond.fps/cond.temp_bandwidth);  % twice the normal width
+                    kernel = hamming(semi*2+1);
+                case 'half-hamming'                    
+                    semi = round(cond.fps/cond.temp_bandwidth*2);  % twice the normal width
+                    kernel = hamming(semi*2+1);
+                    kernel = kernel(semi+1:end);
+                otherwise
+                    error 'unknown temporal kernel'
+            end
+            kernel = kernel/sum(kernel)/sqrt(cond.temp_bandwidth/4);
             
-            % upsample and interpolate
-            factor = cond.pattern_upscale;
+            
+            pad = 3;  % image padding to avoid edge correlations
+            r = RandStream.create('mt19937ar','NormalTransform', ...
+                'Ziggurat', 'Seed', cond.rng_seed);
+            m = r.randn(round(cond.pattern_width/cond.pattern_aspect)+pad, ...
+                cond.pattern_width+pad, ...
+                round(cond.duration*cond.fps) + length(kernel)-1);            
+            m = convn(m, permute(kernel, [3 2 1]), 'valid');
+            
+            % upsample in space
+            factor = 3;
             m = upsample(permute(m, [2 1 3]), factor, round(factor/2))*factor;
             m = upsample(permute(m, [2 1 3]), factor, round(factor/2))*factor;
             sz = size(m);
             
-            
-            % apply directions and offsets
+            % compute directions 
+            period = cond.duration/cond.n_dirs;
             cond.directions = (r.randperm(cond.n_dirs)-1)/cond.n_dirs*360;
             t = (0:sz(3)-1)/cond.fps;
             cond.onsets = ((0.5:cond.n_dirs) - cond.ori_fraction/2)*period;
@@ -104,21 +116,22 @@ classdef Monet2 < dj.Manual & stimulus.core.Visual
             m = fft2(m);
             motion = 1;
             speed = cond.pattern_width*cond.speed/cond.fps;  % in pattern widths per frame
-            mix = cond.ori_mix * (cond.ori_coherence > 1);
             for i = 1:sz(3)
                 fmask = motion.*finterp;  % apply motion first so technically motion starts in next frame
                 if ~isnan(direction(i))
                     ori = direction(i)*pi/180+pi/2;   % following clock directions
                     theta = mod(atan2(fx,fy) + ori, pi) - pi/2;
+                    mix = cond.ori_mix * (cond.ori_coherence > 1);
                     fmask = fmask.*(1-mix + mix*sqrt(cond.ori_coherence).*hann(theta*cond.ori_coherence));
                     motion = motion .* exp(1j*speed*(cos(ori).*fx + sin(ori).*fy));   % negligible error accumulates
                 end
                 m(:,:,i) = fmask.*m(:,:,i);
             end
             m = real(ifft2(m))*2.5;
-            cond.movie = uint8((real(m)+0.5)*255);
+            cond.movie = uint8((m(1:floor((end-pad*factor)/2)*2,1:floor((end-pad*factor)/2)*2,:)+0.5)*255);
         end
     end
+    
     
     methods
         function showTrial(self, cond)
@@ -126,14 +139,7 @@ classdef Monet2 < dj.Manual & stimulus.core.Visual
             assert(~isempty(self.fps), 'Cannot obtain the refresh rate')
             assert(abs(self.fps - cond.fps)/cond.fps < 0.05, 'incorrect monitor frame rate')
             assert((self.rect(3)/self.rect(4) - cond.pattern_aspect)/cond.pattern_aspect < 0.05, 'incorrect pattern aspect')
-            
-            % blank the screen if there is a blanking period
-            if cond.pre_blank_period>0
-                opts.logFlips = false;
-                self.flip(struct('checkDroppedFrames', false))
-                WaitSecs(cond.pre_blank_period);
-            end
-            
+                        
             % play movie
             opts.logFlips = true;
             for i=1:size(cond.movie,3)
