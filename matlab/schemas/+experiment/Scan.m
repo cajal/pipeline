fa3a53c22703e58eb83b66c1e6f1415da93ae548
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2017-04-24 13:01:56 -0500

    added migration script to stimulus.Clip

--- matlab/schemas/+experiment/Scan.m
+++ matlab/schemas/+experiment/Scan.m
@@ -1,19 +1,19 @@
 %{
-experiment.Scan (manual) # scanimage scan info
+# scanimage scan info
 -> experiment.Session
-scan_idx        : smallint               # number of TIFF stack file
+scan_idx                    : smallint                      # number of TIFF stack file
 ---
 -> experiment.Lens
 -> experiment.BrainArea
 -> experiment.Aim
--> experiment.Software
 filename                    : varchar(255)                  # file base name
 depth=0                     : int                           # manual depth measurement
 scan_notes                  : varchar(4095)                 # free-notes
 site_number=0               : tinyint                       # site number
+-> experiment.Software
 scan_ts=CURRENT_TIMESTAMP   : timestamp                     # don't edit
 %}
 
 
-classdef Scan < dj.Relvar
+classdef Scan < dj.Manual
 end
