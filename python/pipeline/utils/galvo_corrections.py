4dd15c47202b2c466dacbe2c5b0cb07fa4989ac7
ecobost <emcobost@gmail.com>
2017-05-03 23:50:20 -0500

    Pipeline working with new scanreader, placeholders in place for motion and raster correction

--- python/pipeline/utils/galvo_corrections.py
+++ python/pipeline/utils/galvo_corrections.py
@@ -1,16 +1,58 @@
 from .. import PipelineException
 from scipy.interpolate import interp1d, RectBivariateSpline, interp2d
+from scipy import signal
 import numpy as np
+
+def compute_motion_offsets(field, template):
+    """ Compute offsets in x and y for rigid subpixel motion correction.
+    
+    :param np.array template: 2-d template image. Each frame in field is aligned to this.
+    :param np.array field: 3-d field (image_height, image_width, num_frames). First two
+        dimensions 
+    
+    :returns: Array with x, y motion offsets for each image in field ([2 x num_frames]).
+        Positive offsets for right and down offsets, e.g., x_offset = 1 means image is 1 
+        pixel to the right of template
+    :rtype: np.array 
+    """
+    # Get some params
+    num_frames = field.shape[2]
+    max_y_offset = int(round(field.shape[0] * 0.10)) # max num_pixels images can be moved in y
+    max_x_offset = int(round(field.shape[1] * 0.10))
+
+    # If template and images are int, signal.correlate fails
+    template = template.astype(np.float32)
+
+    y_offsets = np.empty(num_frames)
+    x_offsets = np.empty(num_frames)
+    for i in range(num_frames):
+
+        # Align pixels (convolving on template and finding highest value)
+        center_patch = field[max_y_offset: -max_y_offset, max_x_offset: -max_x_offset, i]
+        conv_template = signal.correlate(template, center_patch, mode='valid')
+        y_offset, x_offset = np.unravel_index(np.argmax(conv_template), conv_template.shape)
+        y_offsets[i] = y_offset - max_y_offset
+        x_offsets[i] = x_offset - max_x_offset
+
+        # TODO: Subpixel, check for black images, check too much motion
+
+    return y_offsets, x_offsets
+
+
+def compute_raster_phase(image):
+    """"""
+    print('Warning: Raster correction not implemented yet.')
+    #TODO: Compute raster correction
+    return 0
 
 def correct_motion(scan, xy_motion, in_place=True):
     """ Motion correction for two-photon scans.
 
     Moves each image in the scan x number of pixels to the left, y number of pixels up and
-    resamples. Works for 2-d arrays and up. Only for square images (neither interp2d nor
-    RectBivariateSpline can deal with non-rectangular grids).
+    resamples. Works for 2-d arrays and up.
 
     :param np.array scan: Volume with images to be corrected in the first two dimensions.
-    :param list/np.aray xy_motion: Volume with x, y motion offsets for each image in the
+    :param list/np.array xy_motion: Volume with x, y motion offsets for each image in the
     first dimension: usually [2 x num_frames].
     :param bool in_place: If True (default), the original array is modified in memory.
 
@@ -74,7 +116,7 @@ def correct_raster(scan, raster_phase, fill_fraction, in_place=True):
     ne7.ip.correctRaster()
 
     :param np.array scan: Volume with images to be corrected in the first two dimensions.
-    :param float raster_phase: Phase difference beetween odd and even lines.
+    :param float raster_phase: Phase difference between odd and even lines.
     :param float fill_fraction: Ratio between active acquisition and total length of the
     scan line.
     :param bool in_place: If True (default), the original array is modified in memory.
