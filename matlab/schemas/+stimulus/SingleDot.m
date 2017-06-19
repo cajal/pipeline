e321462f535ebf121eed0b4d8567a583882a0159
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2017-06-19 13:31:19 -0500

    minor

--- matlab/schemas/+stimulus/SingleDot.m
+++ matlab/schemas/+stimulus/SingleDot.m
@@ -45,21 +45,15 @@ classdef SingleDot < dj.Manual & stimulus.core.Visual
     
     methods
         function showTrial(self, cond)
-            
             Screen('FillRect', self.win, cond.bg_level, self.rect)
-            
             width = self.rect(3);
             height = self.rect(4);
             tan = height/width;
             x_pos = cond.dot_x + 0.5;
             y_pos = cond.dot_y+0.5*tan;
             rect = [x_pos-cond.dot_xsize/2, y_pos-cond.dot_ysize/2, x_pos+cond.dot_xsize/2, y_pos+cond.dot_ysize/2]*width;
-            switch cond.dot_shape
-                case 'rect'
-                    Screen('FillRect', self.win, cond.dot_level, rect)
-                case 'oval'
-                    Screen('FillOval', self.win, cond.dot_level, rect)
-            end
+            command = struct('rect', 'FillRect', 'oval', 'FillOval');
+            Screen(command.(cond.dot_shape), self.win, cond.dot_level, rect)
             self.flip(struct('clearScreen', false, 'checkDroppedFrames', false))
             WaitSecs(cond.dot_time);
             self.flip(struct('checkDroppedFrames', false))
