80b3c77d39570d37dff5a7107eadb670888ac74a
Jiakun Fu <jiakun.fu.bcm@gmail.com>
2017-09-08 18:04:24 -0500

    add reso pipeline

---
+++ matlab/schemas/+reso/MaskClassification.m
@@ -0,0 +1,21 @@
+%{
+# classification of segmented masks.
+-> reso.SummaryImages
+-> reso.Segmentation
+-> shared.ClassificationMethod
+---
+classif_time=CURRENT_TIMESTAMP: timestamp                   # automatic
+%}
+
+
+classdef MaskClassification < dj.Computed
+
+	methods(Access=protected)
+
+		function makeTuples(self, key)
+		%!!! compute missing fields for key here
+			 self.insert(key)
+		end
+	end
+
+end
