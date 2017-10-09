c676bbba97ec08f03ce8995f51557752e075408a
Minggui Chen <minggui.chen@gmail.com>
2017-10-09 11:32:15 -0500

    meso and reso definitions

---
+++ matlab/schemas/+meso/QualityMeanIntensity.m
@@ -0,0 +1,21 @@
+%{
+# mean intensity values across time
+-> meso.Quality
+-> `pipeline_shared`.`#field`
+-> `pipeline_shared`.`#channel`
+---
+intensities                 : longblob                      # 
+%}
+
+
+classdef QualityMeanIntensity < dj.Computed
+
+	methods(Access=protected)
+
+		function makeTuples(self, key)
+		%!!! compute missing fields for key here
+			 self.insert(key)
+		end
+	end
+
+end
