1fba34dd801babfde12246c3b83fc0c854e02b82
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2017-10-23 17:03:54 -0500

    added fuse.Activity

--- python/pipeline/fuse.py
+++ python/pipeline/fuse.py
@@ -9,17 +9,91 @@ assert StrictVersion(dj.__version__) >= StrictVersion('0.8.2'), "Please upgrade 
 
 schema = dj.schema('pipeline_fuse', locals())
 
+@schema
+class Pipe(dj.Lookup):
+    definition = """
+    # Names of fused pipelines
+    pipe  : varchar(16)    # pipeline name
+    """
+    contents = zip(('reso', 'meso'))
+
+
+
+class Resolver:
+    """
+    This mixin class populates tables that fuse two pipelines
+    """
+
+    def _make_tuples(self, key):
+
+        for pipe, (src, dest) in self.mapping.items():
+            if src() & key:
+                break
+        else:
+            raise PipelineException(
+                    'The key source yielded a key from an uknown pipeline')
+
+        self.insert1(dict(key, pipe=pipe))
+        dest().insert(src() & key, ignore_extra_fields=True)
+
+
+    def resolve(self):
+        """
+        Given a fuse.Activity() object, return the corresponding
+        module.Activity() object and the module itself.
+        :return: (activity,  module) where activity is the module.Activity for the right module
+        """
+        if len(dj.U('pipe') & self) != 1:
+            raise PipelineException('Please restrict query to a single pipeline.')
+        for _, rel in self.mapping.values():
+            if rel:
+                return rel, sys.modules[rel.__module__]
+
 
 @schema
-class ScanDone(dj.Computed):
+class Activity(Resolver, dj.Computed):
     definition = """
-    # Calcium Activity from multiple pipelines
+    # Calcium activity for a single field within a scan
     -> experiment.Scan
     -> shared.PipelineVersion
-    -> shared.SpikeMethod
+    -> shared.Field
+    -> shared.Channel
     -> shared.SegmentationMethod
+    -> shared.SpikeMethod
     ---
-    pipe : char(8)   # meso, reso, etc
+    -> Pipe
+    """
+
+    key_source = meso.Activity().proj() + reso.Activity().proj()
+
+    class Reso(dj.Part):
+        definition = """
+        -> reso.Activity
+        -> Activity
+        """
+
+    class Meso(dj.Part):
+        definition = """
+        -> meso.Activity
+        -> Activity
+        """
+
+    @property
+    def mapping(self):
+        return {'meso': (meso.Activity, Activity.Meso),
+                'reso': (reso.Activity, Activity.Reso)}
+
+
+@schema
+class ScanDone(Resolver, dj.Computed):
+    definition = """
+    # Calcium activity for the whole scan (multiple scan fields)
+    -> experiment.Scan
+    -> shared.PipelineVersion
+    -> shared.SegmentationMethod
+    -> shared.SpikeMethod
+    ---
+    -> Pipe
     """
 
     key_source = meso.ScanDone().proj() + reso.ScanDone().proj()
@@ -36,29 +110,7 @@ class ScanDone(dj.Computed):
         -> ScanDone
         """
 
-    def _make_tuples(self, key):
-
-        for pipe, src, dest in (
-                ('meso', meso.ScanDone, ScanDone.Meso),
-                ('reso', reso.ScanDone, ScanDone.Reso)):
-            if src() & key:
-                break
-        else:
-            raise PipelineException(
-                    'The key source yielded a key from a new pipeline')
-
-        self.insert1(dict(key, pipe=pipe))
-        dest().insert(src() & key, ignore_extra_fields=True)
-
-
-    def resolve(self):
-        """
-        Given a fuse.ScanDone() object, return the corresponding
-        module.ScanDone() object and the module itself.
-        :return: scanDone,  module
-        """
-        if len(dj.U('pipe') & self) != 1:
-            raise PipelineException('cannot query from multiple pipelines at once.  Please narrow down your restriction')
-        scanDone = (reso.ScanDone() & self) or (meso.ScanDone() & self)
-        return scanDone, sys.modules[scanDone.__module__]
-
+    @property
+    def mapping(self):
+        return {'meso': (meso.ScanDone, ScanDone.Meso),
+                'reso': (reso.ScanDone, ScanDone.Reso)}
