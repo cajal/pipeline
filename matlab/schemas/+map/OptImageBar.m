e78afa2e2667c1d2fbb15d71872bd85445bafb99
manolis <olakiril@hotmail.com>
2017-11-28 21:24:08 -0600

    Updated OptImageBar for new stimulus

--- matlab/schemas/+map/OptImageBar.m
+++ matlab/schemas/+map/OptImageBar.m
@@ -30,17 +30,8 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
                     [Data, data_fs,photodiode_signal, photodiode_fs] = ...
                         getOpticalData(getLocalPath(fullfile(path,name))); % time in sec
                     
-                    disp 'synchronizing...'
-                    % synchronize to stimulus
-                    tuple =  sync(key, photodiode_signal, photodiode_fs);
-                    
                     % calculate frame times
-                    frame_times = tuple.signal_start_time + ...
-                        tuple.signal_duration*(1:size(Data,1))/size(Data,1);
-                    
-                    % import Sync table
-                    tuple.frame_times = frame_times;
-                    makeTuples(map.Sync,tuple)
+                    frame_times =fetch1(stimulus.Sync & key,'frame_times');
                     
                     % get the vessel image
                     disp 'getting the vessels...'
@@ -48,10 +39,11 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
                     k.session = key.session;
                     k.animal_id = key.animal_id;
                     k.site_number = fetch1(experiment.Scan & key,'site_number');
-                    vesObj = experiment.Scan & k & 'software = "imager" and aim = "vessels"';
+                    vesObj = experiment.Scan - experiment.ScanIgnored & k & 'software = "imager" and aim = "vessels"';
                     if ~isempty(vesObj)
-                        name = fetch1( vesObj ,'filename');
-                        if isempty(strfind(name,'.h5')); name = [name '.h5'];end
+                        names = fetchn( vesObj ,'filename');
+                        name = names{end};
+                        if ~contains(name,'.h5'); name = [name '.h5'];end
                         filename = getLocalPath(fullfile(path,name));
                         vessels = squeeze(mean(getOpticalData(filename)));
                     end
@@ -60,26 +52,28 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
                     
                     % get Optical data
                     disp 'loading movie...'
-                    reader = preprocess.getGalvoReader(key);
-                    Data = squeeze(mean(reader(:,:,1,:,:),4));
-                    Data = permute(Data,[3 1 2]);
-                    [nslices, data_fs] = fetch1(preprocess.PrepareGalvo & key,'nslices','fps');
+                    if strcmp(setup,'2P4') % mesoscope
+                        path = getLocalPath(fullfile(path, sprintf('%s*.tif', name)));
+                        reader = ne7.scanreader.readscan(path,'int16',1);
+                        Data = permute(squeeze(mean(reader(:,:,:,:,:))),[3 1 2]);
+                        data_fs = reader.fps;
+                        nslices = length(reader.fieldDepths);
+                    else
+                        reader = preprocess.getGalvoReader(key);
+                        Data = squeeze(mean(reader(:,:,1,:,:),4));
+                        Data = permute(Data,[3 1 2]);
+                        [nslices, data_fs] = fetch1(preprocess.PrepareGalvo & key,'nslices','fps');
+                    end
                     
                     % calculate frame times
-                    frame_times = fetch1(preprocess.Sync & key,'frame_times');
+                    frame_times = fetch1(stimulus.Sync & key,'frame_times');
                     frame_times = frame_times(1:nslices:end);
+                    frame_times = frame_times(1:size(Data,1));
                     
                     % get the vessel image
                     try
                         disp 'getting the vessels...'
-                        k = [];
-                        k.session = key.session;
-                        k.animal_id = key.animal_id;
-                        k.site_number = fetch1(experiment.Scan & key,'site_number');
-                        ves_key = fetch(experiment.Scan & k & 'aim = "vessels"');
-                        reader = preprocess.getGalvoReader(ves_key);
-                        vessels = reader(:,:,:,:,:);
-                        vessels = mean(vessels(:,:,:),3);
+                        vessels = squeeze(mean(Data(:,:,:)));
                     catch
                         vessels = [];
                     end
@@ -146,7 +140,7 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
         end
     end
     
-    methods
+  methods
         
         function  [iH, iS, iV] = plot(obj,varargin)
             
@@ -157,9 +151,11 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
             % MF 2012, MF 2016
             
             params.sigma = 2; %sigma of the gaussian filter
-            params.saturation = 1; % saturation scaling 
-            params.exp = []; % exponent factor of rescaling, 1-2 works 
+            params.saturation = 1; % saturation scaling
+            params.exp = []; % exponent factor of rescaling, 1-2 works
             params.shift = 0; % angular shift for improving map presentation
+            params.subplot = true;
+            params.vcontrast = 1;
             params.figure = [];
             
             params = getParams(params,varargin);
@@ -172,7 +168,7 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
             if isempty(keys); disp('Nothing found!'); return; end
             
             for ikey = 1:length(keys)
-             
+                
                 % get data
                 [imP, vessels, imA] = fetch1(obj & keys(ikey),'ang','vessels','amp');
                 
@@ -197,10 +193,10 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
                 imA(imA>prctile(imA(:),99)) = prctile(imA(:),99);
                 
                 % create the hsv map
-                h = imgaussian(normalize(imP),params.sigma);
-                s = imgaussian(normalize(imA),params.sigma)*params.saturation;
+                h = imgaussfilt(normalize(imP),params.sigma);
+                s = imgaussfilt(normalize(imA),params.sigma)*params.saturation;
                 v = ones(size(imA));
-                if ~isempty(vessels); v = normalize(vessels);end
+                if ~isempty(vessels); v = normalize(abs(normalize(vessels).^params.vcontrast));end
                 
                 if nargout>0
                     iH{ikey} = h;
@@ -219,7 +215,11 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
                     % plot
                     angle_map = hsv2rgb(cat(3,h,cat(3,ones(size(s)),ones(size(v)))));
                     combined_map = hsv2rgb(cat(3,h,cat(3,s,v)));
-                    imshowpair(angle_map,combined_map,'montage')
+                    if params.subplot
+                        imshowpair(angle_map,combined_map,'montage')
+                    else
+                        imshow(angle_map)
+                    end
                 end
             end
             
@@ -239,33 +239,252 @@ classdef OptImageBar < dj.Relvar & dj.AutoPopulate
             if isempty(keys); disp('Nothing found!'); return; end
             
             for ikey = 1:length(keys)
-            
+                
                 [h,s,v] = plot(obj & keys(ikey),params);
-
+                
                 im = ones(size(h,1)*2,size(h,2)*2,3);
-
-                im(1:size(h,1),1:size(h,2),1) = h;
-                im(1:size(h,1),1:size(h,2),2) = s;
+                
+                im(1:size(h,1),1:size(h,2),1) = zeros(size(v));
+                im(1:size(h,1),1:size(h,2),2) = zeros(size(v));
                 im(1:size(h,1),1:size(h,2),3) = v;
-
+                
                 im(size(h,1)+1:end,1:size(h,2),1) = zeros(size(v));
                 im(size(h,1)+1:end,1:size(h,2),2) = zeros(size(v));
                 im(size(h,1)+1:end,1:size(h,2),3) = v;
-
+                
                 im(1:size(h,1),size(h,2)+1:end,1) = h;
-                im(1:size(h,1),size(h,2)+1:end,2) = ones(size(h));
-                im(1:size(h,1),size(h,2)+1:end,3) = ones(size(h));
-
-                im(size(h,1)+1:end,size(h,2)+1:end,1) = zeros(size(v));
-                im(size(h,1)+1:end,size(h,2)+1:end,2) = zeros(size(v));
+                im(1:size(h,1),size(h,2)+1:end,2) = s;
+                im(1:size(h,1),size(h,2)+1:end,3) = v;
+                
+                im(size(h,1)+1:end,size(h,2)+1:end,1) = h;
+                im(size(h,1)+1:end,size(h,2)+1:end,2) = ones(size(h));
                 im(size(h,1)+1:end,size(h,2)+1:end,3) = ones(size(h));
-
+                
                 figure
                 set(gcf,'NumberTitle','off','name',sprintf(...
-                        'OptMap direction:%s animal:%d session:%d scan:%d',...
-                        keys(ikey).axis,keys(ikey).animal_id,keys(ikey).session,keys(ikey).scan_idx))
+                    'OptMap direction:%s animal:%d session:%d scan:%d',...
+                    keys(ikey).axis,keys(ikey).animal_id,keys(ikey).session,keys(ikey).scan_idx))
                 imshow(hsv2rgb(im))
+                
+                % contour
+                hold on
+                contour(h,'showtext','on','linewidth',1,'levellist',0:0.05:1)
             end
+        end
+        
+        function [area_map, grad_diff, vessels] = extractAreas(obj,varargin)
+            %%
+            params.init_gauss = 10;
+            params.grad_gauss = 0.1; % initial map gauss filter in sd parameter
+            params.diff_gauss =10; % gradient diff map gauss filter in sd parameter
+            params.diff_open = 10; % gradient diff imopen param
+            params.sign_thr = 1; % threshold as standard deviations of signmap
+            params.step = .01; % mask expand step parameter
+            params.min_area_size = 1000;  % min area size in pixels
+            params.final_erode = 10;
+            params.final_dilate = 10;
+            params.exp = 2;
+            
+            params = getParams(params,varargin);
+            
+            % find horizontal & verical map keys
+            Hkeys = fetch(map.OptImageBar & (experiment.Session & obj) & 'axis="horizontal"');
+            Vkeys = fetch(map.OptImageBar & (experiment.Session & obj) & 'axis="vertical"');
+            
+            % get vessels
+            vessels = normalize(fetch1(map.OptImageBar & Hkeys(end),'vessels'));
+            
+            % fetch horizontal & vertical maps
+            [H, A1] = fetch1(map.OptImageBar & Hkeys(end),'ang','amp');
+            [Hor(:,:,1),Hor(:,:,2),Hor(:,:,3)] = plot(map.OptImageBar & Hkeys(end),'exp',params.exp,'sigma',params.init_gauss);
+            [V,A2] = fetch1(map.OptImageBar & Vkeys(end),'ang','amp');
+            [Ver(:,:,1),Ver(:,:,2),Ver(:,:,3)] = plot(map.OptImageBar & Vkeys(end),'exp',params.exp,'sigma',params.init_gauss);
+            A = (A1+A2)/2;
+            
+            % calculate gradients
+%             [~,dH] = imgradient(H);
+%             [~,dV] = imgradient(V);
+            [~,dH] = imgradient(Hor(:,:,1));
+            [~,dV] = imgradient(Ver(:,:,1));
+            
+            % filter gradients
+            dH = imgaussfilt(dH,params.grad_gauss);
+            dV = imgaussfilt(dV,params.grad_gauss);
+            grad_diff = imopen(imgaussfilt(sind(dH  - dV),params.diff_gauss),strel('disk',params.diff_open));
+            
+            % plot maps
+            clf;%figure
+            subplot(2,2,1)
+            imagesc(hsv2rgb(Hor)); axis image; axis off; title('Horizontal Retinotopy')
+            subplot(2,2,2)
+            imagesc(hsv2rgb(Ver)); axis image; axis off; title('Vertical Retinotopy')
+            subplot(2,2,3)
+            imagesc(grad_diff); axis image; axis off; title('Visual Field Sign Map'); colormap jet
+            
+            %% filter & expand masks
+            filtered_grad_diff = zeros(size(grad_diff));
+            imStd = std(grad_diff(:));
+            filtered_grad_diff(grad_diff>imStd*params.sign_thr)= 1;
+            filtered_grad_diff(grad_diff<-imStd*params.sign_thr)= -1;
+            SE = strel('arbitrary',[0 1 0;1 1 1;0 1 0]); %%% parameter
+            SEMAX = strel('arbitrary',[0 0 1 0 0;0 1 1 1 0;1 1 1 1 1;0 1 1 1 0;0 0 1 0 0]); %%% parameter
+            [all_areas, n] = bwlabel(filtered_grad_diff);
+            for iThr = 1:-params.step:0.05
+                n = max(all_areas(:));
+                thr = imStd*iThr;
+                for iArea = 1:n
+                    one_area = imfill(imdilate(all_areas==iArea,SE),'holes');
+                    all_areas(one_area & all_areas==0 & abs(grad_diff)>thr) = iArea;
+                end
+                lmax = imdilate(imbinarize(all_areas),SEMAX);
+                one_area = bwlabel(abs(grad_diff)>thr & lmax == 0);
+                stats = regionprops(one_area,'area');
+                un = unique(one_area(:));
+                for i = un(un>0)'
+                    if stats(i).Area<params.min_area_size
+                        one_area(one_area==i) = 0;
+                    end
+                end
+                one_area = bwlabel(one_area);
+                if any(one_area(:))
+                    all_areas(logical(one_area)) = one_area(logical(one_area))+n;
+                end
+            end
+            
+            % select areas that have at least half their area with amplitude more than the threshold
+            area_map = zeros(size(all_areas));
+            idx = 0;
+            thr = mean(A(:));% max(A(:)) - 2 * std(A(:)); %%% parameter
+            for iarea = 1:n
+                if ~(sum(all_areas(:)==iarea)/sum(all_areas(:)==iarea & A(:)>thr)>2)
+                    idx = idx+1;
+                    mask = imdilate(imerode(all_areas==iarea,strel('disk',params.final_erode)),strel('disk',params.final_dilate));
+                    area_map(mask) = idx;
+                end
+            end
+            
+            % plot unique areas
+            im(:,:,1) = normalize(area_map);
+            im(:,:,2) = area_map>0;
+            im(:,:,3) = vessels;
+            subplot(2,2,4)
+            image(hsv2rgb(im)); axis image; axis off; title('Areas')
+            
+            %% add numbers to areas
+            stats = struct([]);
+            for iarea = 1:max(area_map(:))
+                s = regionprops(area_map==iarea,'area','Centroid');
+                if iarea==1
+                    stats = s;
+                else
+                    stats(iarea).Area = s.Area;
+                    stats(iarea).Centroid = s.Centroid;
+                end
+                stats(iarea).sign = mean(grad_diff(area_map==iarea))>0;
+                text(stats(iarea).Centroid(1),stats(iarea).Centroid(2),num2str(iarea))
+            end
+        end
+        
+        function area_map = editMask(self, masks, background)
+            if nargin<3
+                images = fetchn(self, 'vessels');
+                background = images{1};
+            end
+            area_map = ne7.ui.paintMasks(background,masks);
+            
+            % add numbers to areas
+            stats = struct([]);idx = 0;
+            areas = unique(area_map(:));
+            for iarea = areas(2:end)'
+                idx = idx+1;
+                s = regionprops(area_map==iarea,'area','Centroid');
+                if isempty(stats)
+                    stats = s;
+                else
+                    stats(idx).Area = s.Area;
+                    stats(idx).Centroid = s.Centroid;
+                end
+                stats(idx).sign = mean(area_map(area_map==iarea))>0;
+                text(stats(idx).Centroid(1),stats(idx).Centroid(2),num2str(idx))
+            end
+        end
+        
+        function insertAreas(obj,area_map,varargin)
+            c = get(gca,'Children');
+            istxt = find(arrayfun(@(x) strcmp(x.Type,'text'),c));
+            p = []; t =[];
+            for itxt = istxt'
+                p(itxt,:) = c(itxt).Position;
+                t{itxt}= c(itxt).String;
+            end
+            
+            areas = fetchn(map.Area,'area');
+            for iarea = 1:max(area_map(:))
+                im = area_map==iarea;
+                idx = im(sub2ind(size(area_map),round(p(:,2)),round(p(:,1))));
+                if all(idx==0);continue;end
+                midx = strcmp(areas,t{idx});
+                if all(midx==0);continue;end
+                key = fetch(experiment.Scan & obj);
+                key.area = areas{midx};
+                key.mask = im;
+                key.slice = 1;
+                insert(map.AreaMask,key)
+            end
+        end
+        
+        function locateRF(obj,varargin)
+            
+            params.grad_gauss = 1;
+            params.scale = 5;
+            params.exp = 1;
+            
+            params = getParams(params,varargin);
+            
+            % find horizontal & verical map keys
+            Hkeys = fetch(map.OptImageBar & (experiment.Session & obj) & 'axis="horizontal"');
+            Vkeys = fetch(map.OptImageBar & (experiment.Session & obj) & 'axis="vertical"');
+                        
+            % fetch horizontal & vertical maps
+            [Hor(:,:,1),Hor(:,:,2),Hor(:,:,3)] = plot(map.OptImageBar & Hkeys(end),'exp',params.exp);
+            [Ver(:,:,1),Ver(:,:,2),Ver(:,:,3)] = plot(map.OptImageBar & Vkeys(end),'exp',params.exp);
+            
+            % get vessels
+            vessels = normalize(Hor(:,:,3));
+            
+            % filter gradients
+            H = roundall(normalize(imgaussfilt(Hor(:,:,1),params.grad_gauss))*params.scale,0.1);
+            V = roundall(normalize(imgaussfilt(Ver(:,:,1),params.grad_gauss))*params.scale,0.1);
+            
+            % plot maps
+            figure
+            subplot(2,3,6)
+            image(hsv2rgb(Ver)); axis image; axis off; title('Vertical Retinotopy')
+            subplot(2,3,3)
+            image(hsv2rgb(Hor)); axis image; axis off; title('Horizontal Retinotopy')
+            hold on
+            subplot(2,3,[1 2 4 5])
+            hold on
+            image(repmat(vessels,1,1,3)); axis image
+            set(gca,'ydir','reverse')
+            
+            set (gcf, 'WindowButtonMotionFcn', {@mouseMove,H,V});
+            
+            function mouseMove (object, eventdata,H,V)
+                global handles
+                try
+                    delete(handles)
+                    
+                    C = get (gca, 'CurrentPoint');
+                    title(gca, ['(X,Y) = (', num2str(C(1,1)), ', ',num2str(C(1,2)), ')']);
+                    Hv = H(round(C(1,2)),round(C(1,1)));
+                    Vv = V(round(C(1,2)),round(C(1,1)));
+                    I = find(H'==Hv & V'==Vv);
+                    [x,y] = ind2sub(size(H),I);
+                    handles = plot(x,y,'.r');
+                end
+            end
+            
         end
     end
     
