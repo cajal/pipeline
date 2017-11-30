6e5a096273e13b6006c9dcb2792f72e861831b6b
manolis <olakiril@hotmail.com>
2017-11-30 08:33:36 -0600

    pop stats

--- matlab/schemas/+movies/PopStats.m
+++ matlab/schemas/+movies/PopStats.m
@@ -3,7 +3,7 @@
 -> fuse.ScanDone
 -> stimulus.Clip
 ---
-frame_mean             : mediumblob      # frame mean
+act_mean             : float      #  mean activity
 %}
 
 classdef PopStats < dj.Imported
@@ -40,23 +40,8 @@ classdef PopStats < dj.Imported
             % subsample traces
             fps = 1/median(diff(flip_times(1,:)));
             d = max(1,round(bin/1000*fps));
-            traces = convn(permute(X(flip_times - caTimes(1)),[2 3 1]),ones(d,1)/d,'same');
-            traces = traces(1:d:end,:,:);
-            traces = permute(X(flip_times - caTimes(1)),[2 3 1]);
-            
-            % split for unique stimuli
-            for istim = 1:length(Stims)
-                [s_trials,s_clips,s_names] = fetchn(trials*vis.MovieClipCond &...
-                    sprintf('movie_name = "%s"',Stims{istim}),'trial_idx','clip_number','movie_name');
-                [tr_idx, b]= ismember(trial_idxs,s_trials);
-                st_idx = b(b>0);
-                dat = permute(traces(:,:,tr_idx),[2 3 1]);
-                info.bins{istim} = reshape(repmat(1:size(dat,3),size(dat,2),1),1,[]);
-                info.trials{istim} = reshape(repmat(s_trials(st_idx),1,size(dat,3)),1,[]);
-                info.clips{istim} = reshape(repmat(s_clips(st_idx),1,size(dat,3)),1,[]);
-                info.names{istim} = reshape(repmat(s_names(st_idx),1,size(dat,3)),1,[]);
-                Data{istim} = reshape(dat,size(traces,2),[]);
-            end
+            Data = convn(permute(X(flip_times - caTimes(1)),[2 3 1]),ones(d,1)/d,'same');
+            Data = Data(1:d:end,:,:);            
         end
     end
     
