283df9687bb0b796a9bf2194a55a8976539059f9
manolis <olakiril@hotmail.com>
2018-01-25 15:28:30 -0600

    Jobs support

--- matlab/schemas/+movies/ClipIgnore.m
+++ matlab/schemas/+movies/ClipIgnore.m
@@ -8,18 +8,30 @@ notes                : varchar(256)             # a little explanation for the e
 
 classdef ClipIgnore < dj.Manual
     methods
-        function inspectClip(obj, keys, fps)
-            if nargin<3
-                fps = 30;
-            end
+        function inspectClip(obj, restrict_key, fps)
+            if nargin<2; restrict_key = []; end
+            if nargin<3; fps = 30; end
+            keys = [];
             
-            keys = fetch(stimulus.MovieClip - obj - movies.ClipInspected & keys);
+            % fetch unviewed keys
+            updateKeys
             
             % loop through clips and replace if necessary
-            for ikey = 1:length(keys)
+            while ~isempty(keys)
                 
                 % init
-                key = keys(ikey);
+                key = keys(1);
+                
+                % insert as a job
+                tuple = key;
+                tuple.judge = 'human';
+                tuple.notes = sprintf('Watched clip at %d fps',fps);
+                if ~exists(movies.ClipInspected & tuple)
+                    insert(movies.ClipInspected, tuple)
+                else
+                    updateKeys
+                    continue
+                end
                 fprintf('Inspecting clip: %s \n',cell2mat(reshape(...
                     [fieldnames(key) repmat({':'},2,1) cellfun(@num2str,struct2cell(key),'uni',0) repmat({'  '},2,1) ]',[],1)'))
                 
@@ -31,18 +43,34 @@ classdef ClipIgnore < dj.Manual
                 if strcmp('yes', answer)
                     key.judge = 'human';
                     key.notes = 'Failed manual inspection';
-                    insert(obj, key)
+                    if ~exists(obj & key)
+                        insert(obj, key)
+                    end
+                    if exists(movies.ClipInspected & key)
+                        delQuick(movies.ClipInspected & key);
+                    end
                 elseif strcmp('no', answer)
                     key.judge = 'human';
                     key.notes = sprintf('Watched clip at %d fps',fps);
-                    insert(movies.ClipInspected, key)
+                    if ~exists(movies.ClipInspected & key)
+                        insert(movies.ClipInspected, key)
+                    end
                 elseif strcmp('cancel', answer)
                     fh.close
+                    if exists(movies.ClipInspected & key)
+                        delQuick(movies.ClipInspected & key);
+                    end
                     return
                 end
                 
                 % close figure
                 fh.close
+                updateKeys
+            end
+            
+            function updateKeys
+                % update unviewed keys
+                keys = fetch((stimulus.MovieClip - obj - movies.ClipInspected) & restrict_key);
             end
         end
     end
