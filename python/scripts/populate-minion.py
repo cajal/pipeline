9518884543efa88b973c85b9a2c56715d7608151
ecobost <emcobost@gmail.com>
2018-01-27 20:22:38 -0600

    add pixelwise maps to autopopulate

--- python/scripts/populate-minion.py
+++ python/scripts/populate-minion.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python
-from pipeline import reso, meso, stack
+from pipeline import reso, meso, fuse, stack
 from pipeline import experiment
 import time
+import warnings
 
 while True:
     # Scans
@@ -19,6 +20,34 @@ while True:
             pipe.Activity().populate(next_scans, {'spike_method': 5}, reserve_jobs=True, suppress_errors=True)
             full_scans = (pipe.ScanInfo().proj() & pipe.Activity()) - (pipe.ScanInfo.Field() - pipe.Activity())
             pipe.ScanDone().populate(next_scans & full_scans, reserve_jobs=True, suppress_errors=True)
+
+        # fuse
+        fuse.MotionCorrection().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.ScanSet().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.Activity().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.ScanDone().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+        # tune (some of this are memory intensive)
+        try:
+            from stimline import tune
+        except ImportError:
+            warnings.warn('Warning: Skipping pixelwise maps. Install stimulus (cajal/stimuli)'
+                          ' and stimline (cajal/stimulus-pipeline).')
+        else:
+            #stimulus.Sync needs to be ran from Matlab
+            tune.STA().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.STAQual().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.TimedSTA().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.TimedSTAQual().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+            tune.CaMovie().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.Drift().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.OriDesign().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.OriMap().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.Cos2Map().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            tune.OriMapQuality().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+            tune.OracleMap().populate(next_scans, reserve_jobs=True, suppress_errors=True)
 
     # Stacks
     stack.StackInfo().populate(stack.CorrectionChannel(), reserve_jobs=True, suppress_errors=True) #TODO: stackAutoProcessing
