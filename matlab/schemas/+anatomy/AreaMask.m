0c8df7c7336daa7c2ef0ff58305711508e793af7
manolis <olakiril@hotmail.com>
2018-01-29 17:48:07 -0600

    code refinement

--- matlab/schemas/+anatomy/AreaMask.m
+++ matlab/schemas/+anatomy/AreaMask.m
@@ -32,6 +32,7 @@ classdef AreaMask < dj.Manual
             % get masks already extracted
             if exists(obj & rmfield(key,'ret_idx'))
                 [area_map, keys] = getContiguousMask(obj, rmfield(key,'ret_idx'));
+                
             else
                 area_map = zeros(size(background,1),size(background,2));
             end
@@ -57,7 +58,8 @@ classdef AreaMask < dj.Manual
             
             % loop through all areas get area name and insert
             areas = unique(area_map(:));
-            for iarea = areas(2:end)'
+            areas = areas(areas>0);
+            for iarea = areas'
                 
                 % fix saturation for selected area
                 colors =  0.2*(area_map>0);
@@ -121,64 +123,63 @@ classdef AreaMask < dj.Manual
             params.exp = 0.4;
             
             params = ne7.mat.getParams(params,varargin);
-            
-            % get mask info
-            [masks, areas] = fetchn(obj,'mask','brain_area');
-            
-            % identify mask areas
-            area_map = zeros(size(masks{1}));
-            for imasks = 1:length(masks)
-                area_map(masks{imasks}) = imasks;
-            end
-            
-            if exists(fuse.ScanDone & obj)
-                % fetch 2p avg image
-                if strcmp(fetch1(experiment.Session & obj, 'rig'),'2P4')
-                    background = fetch1(meso.SummaryImagesAverage & (fuse.ScanSet & obj),...
-                        'average_image');
-                else
-                    background = fetch1(reso.SummaryImagesAverage & (fuse.ScanSet & obj),...
-                        'average_image');
+
+            if exists(map.RetMap & (map.RetMapScan &  obj))
+                
+                % get mask info
+                [masks, areas] = fetchn(obj,'mask','brain_area');
+                
+                % identify mask areas
+                area_map = zeros(size(masks{1}));
+                for imasks = 1:length(masks)
+                    area_map(masks{imasks}) = imasks;
                 end
-            else
+                
                 % get maps
                 background = getBackground(map.RetMap & (map.RetMapScan &  obj));
                 
                 % if FieldCoordinates exists add it to the background
-                if exists(anatomy.FieldCoordinates & (mice.Mice & obj))
-                    background = cat(4,background,plot(anatomy.FieldCoordinates &...
+                if exists(anatomy.FieldCoordinates & proj(anatomy.RefMap & obj))
+                    background = cat(4,background,plot(anatomy.FieldCoordinates & ...
                         (anatomy.RefMap & obj)));
                 end
+            else
+                [area_map, keys, background] = getContiguousMask(obj,fetch(obj));
+                areas = {keys(:).brain_area}';
             end
             
             % adjust background contrast
-            background = ne7.mat.normalize(abs(ne7.mat.normalize(background).^ params.exp));
+            background = ne7.mat.normalize(abs(background.^ params.exp));
             
             % merge masks with background
-            im = hsv2rgb(cat(3,ne7.mat.normalize(area_map),area_map>0,background(:,:,1,1)));
+            im = hsv2rgb(cat(3,ne7.mat.normalize(area_map),background(:,:,1,1),background(:,:,1,1)));
             if nargin<2 || isempty(params.back_idx) || params.back_idx > size(background,4)
                 image((im));
             else
                 imshowpair(im,background(:,:,:,params.back_idx),'blend')
             end
+            axis image;
+            key = fetch(proj(experiment.Scan) & obj);
+            set(gcf,'name',sprintf('Animal:%d Session:%d Scan:%d',key.animal_id,key.session,key.scan_idx))
             
             % place area labels
-            for iarea = 1:length(masks)
-                s = regionprops(masks{iarea},'Centroid');
-                text(s(1).Centroid(1),s(1).Centroid(2),areas{iarea})
+            un_areas = unique(areas);
+            for iarea = 1:length(un_areas)
+                s = regionprops(area_map==iarea,'Centroid');
+                text(s(1).Centroid(1),s(1).Centroid(2),un_areas{iarea},'color',[1 0 0],'fontsize',16)
             end
         end
         
-        function [area_map, keys] = getContiguousMask(obj, key)
+        function [area_map, keys, background] = getContiguousMask(obj, key)
             
             % fetch masks & keys
             [masks, keys] = fetchn(obj & key,'mask');
             
             % get information from the scans depending on the setup
             if strcmp(fetch1(experiment.Session & key,'rig'),'2P4') || length(masks)<2
-                [x_pos, y_pos, fieldWidths, fieldHeights, fieldWidthsInMicrons, masks, areas] = ...
+                [x_pos, y_pos, fieldWidths, fieldHeights, fieldWidthsInMicrons, masks, areas, avg_image] = ...
                     fetchn(obj * meso.ScanInfoField * meso.SummaryImagesAverage & key,...
-                    'x','y','px_width','px_height','um_width','mask','brain_area');
+                    'x','y','px_width','px_height','um_width','mask','brain_area','average_image');
                 
                 % calculate initial scale
                 pxpitch = mean(fieldWidths.\fieldWidthsInMicrons);
@@ -187,12 +188,14 @@ classdef AreaMask < dj.Manual
                 x_pos = (x_pos - min(x_pos))/pxpitch;
                 y_pos = (y_pos - min(y_pos))/pxpitch;
                 area_map = zeros(ceil(max(y_pos+fieldHeights)),ceil(max(x_pos+fieldWidths)));
+                background = zeros(size(area_map));
                 for islice =length(masks):-1:1
                     mask = double(masks{islice})*find(strcmp(areas{islice},unique(areas)));
                     y_idx = ceil(y_pos(islice)+1):ceil(y_pos(islice))+size(mask,1);
                     x_idx = ceil(x_pos(islice)+1):ceil(x_pos(islice))+size(mask,2);
                     back = area_map(y_idx, x_idx);
                     area_map(y_idx, x_idx) = max(cat(3,mask,back),[],3);
+                    background(y_idx, x_idx) = avg_image{islice};
                 end
                 
             else
