ca882f0904cf5f973542561a54027209980a8eec
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2018-03-07 20:08:04 -0600

    fix pipetools.ts2sec to deal with interrupted recordings

--- matlab/lib/+pipetools/ts2sec.m
+++ matlab/lib/+pipetools/ts2sec.m
@@ -1,41 +1,19 @@
-function s = ts2sec(ts,packetLen)
+function ts = ts2sec(ts)
+% convert 10 MHz counts from the patching program (ts) to seconds 
+% return: timestamps converted to seconds for each sample in the ts
 
-% convert 10MHz timestamps from Saumil's patching program (ts) to seconds (s)
-% s: timestamps converted to seconds
-% packetLen: length of timestamped packets 
-% start: system time (in seconds) of t=0;
-% badInd: bad camera indices from 2^31:2^32 in camera timestamps prior to 4/10/13
+% remove wraparound and convert to seconds
+rate = 1e7;  % Hz
+ts = double(ts(:));
+ts = [ts(1); ts(1) + cumsum(mod(diff(ts), 2^32))] / rate;
 
-if nargin==1
-    packetLen = 0;
-end
-
-ts=double(ts);
-
-%% check for random issue
-assert(sum(ts==2^31-1) < 10,'Check for old bad timestamp issue failed')
-
-%%  remove wraparound
-wrapInd = find(diff(ts)<0);
-while ~isempty(wrapInd)
-    ts(wrapInd(1)+1:end)=ts(wrapInd(1)+1:end)+2^32;
-    wrapInd = find(diff(ts)<0);
-end
-    
-%% convert to seconds 
-s = ts/1E7;
-
-%% Remove offset, and if not monotonically increasing (i.e. for packeted ts), interpolate
-if any(diff(s)<=0)
-    % Check to make sure it's packets
-    assert(packetLen == find(diff(s)>0,1,'first'));
-    
-    % Interpolate
-    nonZero = [1 ; find(diff(s)>0)+1];
-    s=interp1(nonZero,s(nonZero),1:length(s),'linear','extrap');
-end
-
-%% reshape if necessary
-if size(s) ~= size(ts)
-    s=s';
-end
+% Interpolate excluding areas with anomalies
+timed = find(diff([-inf; ts]));
+assert(max(diff(timed))==min(diff(timed)), 'unequal packet sizes')
+packet_duration = median(diff(ts(timed)));
+ix = find(diff(ts(timed)) < packet_duration / 1.1 | ...
+    diff(ts(timed)) > packet_duration * 1.1);
+warning('Found %d interruptions in packets', length(ix))
+ts(timed(ix))=nan;
+ts(timed(ix+1))=nan;
+ts = interp1(timed, ts(timed), 1:length(ts));
