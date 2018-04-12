bd76c03e668d99fb49440a592a76f94be46d3b7f
ecobost <emcobost@gmail.com>
2018-04-12 18:43:17 -0500

    slightly more stringent threshold to count packets as mistimed (20 msecs)

--- python/pipeline/utils/h5.py
+++ python/pipeline/utils/h5.py
@@ -37,8 +37,8 @@ def ts2sec(ts, sampling_rate=1e7, is_packeted=False):
         ts_secs = np.interp(sample_xs, xs, ys)
 
         # Invalidate timepoints with unequal spacing between packets
-        if np.any(abs(np.diff(ys) - expected_length) > 0.15 * expected_length):
-            abnormal_diffs = abs(np.diff(ys) - expected_length) > 0.15 * expected_length
+        if np.any(abs(np.diff(ys) - expected_length) > 0.1 * expected_length):
+            abnormal_diffs = abs(np.diff(ys) - expected_length) > 0.1 * expected_length
             abnormal_limits = np.where(np.diff([0, *abnormal_diffs, 0]))[0]
             for start, stop in zip(abnormal_limits[::2], abnormal_limits[1::2]):
                 abnormal_indices = np.logical_and(sample_xs > xs[start], sample_xs < xs[stop])
