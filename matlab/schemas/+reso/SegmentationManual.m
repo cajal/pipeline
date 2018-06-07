276eb60f92124e7612ba6b87ac61600473bbde14
manolis <olakiril@hotmail.com>
2018-06-07 17:01:59 -0500

    only insert masks

--- matlab/schemas/+reso/SegmentationManual.m
+++ matlab/schemas/+reso/SegmentationManual.m
@@ -2,58 +2,37 @@
 # masks created manually
 -> reso.Segmentation
 ---
-mask                        : longblob                      # binary 4-connected mask image segmenting the aligned image
-segment_ts=CURRENT_TIMESTAMP: timestamp                     # automatic
 %}
 
 
 classdef SegmentationManual < dj.Computed
     
     properties
-        popRel  = reso.Segmentation & reso.SummaryImagesAverage
+        popRel  = reso.SummaryImagesAverage
     end
     
     methods(Access=protected)
         function makeTuples(self, key)
-            images = fetchn(reso.SummaryImagesAverage & key, 'average_image');
-            if verLessThan('matlab', '9.1')
-                warning('You are running an older version of Matlab, switchin to the old segmenation code!')
-                bw = preprocess.ManualSegment.outlineCells(images);
-            else
-                bw = preprocess.ManualSegment.paintCells(images);
-            end
-            assert(~isempty(bw), 'user aborted segmentation')
-            key.mask = bw;
+            images = fetch1(reso.SummaryImagesAverage & key, 'average_image');
+            masks = ne7.ui.paintMasks(images);
+            assert(~isempty(masks), 'user aborted segmentation')
+            key.segmentation_method = 1;
+            r_key = rmfield(key,'pipe_version');
+            r_key.compartment = 'unknown';
+            insert(reso.SegmentationTask,r_key);
+            insert(reso.Segmentation,key);
             self.insert(key)
+            
+            % Insert Masks
+            unique_masks = unique(masks);
+            key.mask_id = 0;
+            for mask = unique_masks(unique_masks>0)'
+                key.mask_id = key.mask_id+1;
+                key.pixels = find(masks==mask);
+                key.weights = ones(size(key.pixels));
+                insert(reso.SegmentationMask,key)
+            end    
         end
     end
-    
-    methods(Static)
-        function bw = outlineCells(images, bw)
-            if ~exist('bw','var')
-                bw = false(size(images{1}));
-            end
-            f = figure;
-            if length(images)==2
-                imshowpair(sqrt(images{1}), sqrt(images{2}))
-            else
-                template = sqrt(images{1});
-                template = template - min(template(:));
-                template = template / max(template(:));
-                imshow(template)
-            end
-            set(gca, 'Position', [0.05 0.05 0.9 0.9]);
-            if strcmp(computer,'GLNXA64')
-                set(f,'Position',[160 160 1400 1000])
-            end
-            bw = ne7.ui.drawCells(bw);
-            close(f)
-        end
-        
-        function bw = paintCells(images)
-            bw = ne7.ui.paintMasks(images{1});
-        end
-    end
-    
     
 end
