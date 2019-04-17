686c8af38e0ac0a43e67c00c4f278285924968ed
Donnie Kim <kdk411@gmail.com>
2019-04-17 18:20:08 -0500

    test script for autoporcessing with DLC tracker

---
+++ python/scripts/test_script.py
@@ -0,0 +1,85 @@
+#!/usr/local/bin/python3
+import time
+
+from pipeline import experiment, reso, meso, fuse, stack, pupil, pupil_new, treadmill, posture
+from stimulus import stimulus
+from stimline import tune
+
+while True:
+    # Scans
+    for priority in range(120, -130, -10): # highest to lowest priority
+        next_scans = experiment.AutoProcessing() & 'priority > {}'.format(priority)
+
+        # stimulus
+        stimulus.Sync().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        stimulus.BehaviorSync().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+        # treadmill, pupil, posture
+        treadmill.Treadmill().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        pupil.Eye().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        pupil_new.TrackedLabelsDeeplabcut.populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        pupil_new.FittedContourDeeplabcut.populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        posture.Posture().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+        # stack
+        stack.StackInfo().populate(stack.CorrectionChannel(), reserve_jobs=True, suppress_errors=True)
+        stack.Quality().populate(reserve_jobs=True, suppress_errors=True)
+        stack.RasterCorrection().populate(reserve_jobs=True, suppress_errors=True)
+        stack.MotionCorrection().populate(reserve_jobs=True, suppress_errors=True)
+        stack.Stitching().populate(reserve_jobs=True, suppress_errors=True)
+        stack.CorrectedStack().populate(reserve_jobs=True, suppress_errors=True)
+
+        # reso/meso
+        for pipe in [reso, meso]:
+            pipe.ScanInfo().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.Quality().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.RasterCorrection().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.MotionCorrection().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.SummaryImages().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.Segmentation().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.Fluorescence().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.MaskClassification().populate(next_scans, {'classification_method': 2},
+                                               reserve_jobs=True, suppress_errors=True)
+            pipe.ScanSet().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+            pipe.Activity().populate(next_scans, {'spike_method': 5}, reserve_jobs=True, suppress_errors=True)
+            full_scans = (pipe.ScanInfo().proj() & pipe.Activity()) - (pipe.ScanInfo.Field() - pipe.Activity())
+            pipe.ScanDone().populate(next_scans & full_scans, reserve_jobs=True, suppress_errors=True)
+
+        # fuse
+        fuse.MotionCorrection().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.ScanSet().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.Activity().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+        fuse.ScanDone().populate(next_scans, reserve_jobs=True, suppress_errors=True)
+
+        # more stack (needs corrected fields)
+        stack.PreprocessedStack().populate(stack.SegmentationTask(), reserve_jobs=True,
+                                           suppress_errors=True)
+        # stack.Segmentation().populate(reserve_jobs=True, suppress_errors=True)
+        stack.FieldSegmentation().populate(reserve_jobs=True, suppress_errors=True)
+        stack.PreprocessedStack().populate(stack.RegistrationTask().proj(
+            session='stack_session', channel='stack_channel'), reserve_jobs=True,
+            suppress_errors=True)
+        stack.Registration().populate(reserve_jobs=True, suppress_errors=True)
+
+        # tune (these are memory intensive)
+        tune_scans = next_scans & (experiment.Scan() & 'scan_ts > "2017-12-00 00:00:00"')
+
+        tune.STA().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.STAQual().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.STAExtent().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+
+        tune.CaMovie().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.Drift().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.OriDesign().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.OriMap().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.Cos2Map().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.OriMapQuality().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+
+        #tune.OracleMap().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        #tune.MovieOracle().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        #tune.MovieOracleTimeCourse().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+
+        tune.CaTimes().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+        tune.Ori().populate(tune_scans, reserve_jobs=True, suppress_errors=True)
+
+    time.sleep(600) # wait 10 minutes before trying to process things again
