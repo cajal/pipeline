52de014d2159fbcb28ba824044d34dbf485a6521
ecobost <emcobost@gmail.com>
2019-04-19 12:44:28 -0500

    minor bug: definition pointing to the wrong place

--- python/pipeline/experiment.py
+++ python/pipeline/experiment.py
@@ -6,7 +6,7 @@ import os
 from commons import lab
 
 
-schema = dj.schema('pipeline_experiment', locals(), create_tables=False)
+schema = dj.schema('pipeline_experiment', locals(), create_tables=True)
 
 
 @schema
@@ -298,7 +298,8 @@ class LaserCalibration(dj.Manual):
 
 @schema
 class MonitorCalibration(dj.Manual):
-    definition = """ # Monitor luminance calibration
+    definition = """ # monitor luminance calibration
+    
     -> experiment.Scan
     ---
     pixel_value             : mediumblob      # control pixel value (0-255)
@@ -335,7 +336,8 @@ class MonitorCalibration(dj.Manual):
 
 @schema
 class SurgeryType(dj.Lookup):
-    definition = """ # Surgery types used in experiment.Surgery
+    definition = """ # diff types of surgery
+    
     surgery_type                : varchar(64)         # Types of surgery performed on mice
     """
     contents = [
@@ -350,7 +352,8 @@ class SurgeryType(dj.Lookup):
 
 @schema
 class SurgeryOutcome(dj.Lookup):
-    definition = """ # Surgery outcomes used in experiment.Surgery
+    definition = """ # surgery outcomes
+    
     surgery_outcome             : varchar(32)         # Possible outcomes of surgeries performed on mice
     """
     contents = [
@@ -361,13 +364,13 @@ class SurgeryOutcome(dj.Lookup):
 
 @schema
 class Surgery(dj.Manual):
-    definition = """ # List of surgeries performed on mice
+    definition = """ # surgeries performed on mice
     -> mice.Mice
     surgery_id                   : smallint               # Unique number given to each surgery
     ---
     date                         : date                   # YYYY-MM-DD Format. Date surgery was performed
     time                         : time                   # Start of mouse recovery
-    -> experiment.Person            
+    -> Person            
     -> SurgeryOutcome
     -> SurgeryType
     surgery_quality              : tinyint                # 0-5 self-rating, 0 being worst and 5 best
@@ -379,7 +382,8 @@ class Surgery(dj.Manual):
 
 @schema
 class SurgeryStatus(dj.Manual):
-    definition = """ # Updates to surgeries performed on mice
+    definition = """ # updates to surgeries
+    
     -> Surgery
     timestamp                           : timestamp              # Timestamp of entry
     ---
