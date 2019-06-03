6e2cf5ea016b1c8ba43e00be7188174006099fba
Donnie Kim <kdk411@gmail.com>
2019-06-03 16:42:47 -0500

    track and fit pupil

--- python/scripts/populate-minion_gpu.py
+++ python/scripts/populate-minion_gpu.py
@@ -1,8 +1,11 @@
 #!/usr/local/bin/python3
-import time 
+import time
 
-from pipeline import stack
+from pipeline import stack, pupil
 
-while True: 
-    stack.Segmentation.populate(reserve_jobs=True, suppress_errors=True) 
-    time.sleep(3600) # wait an hour before checking again
+while True:
+    stack.Segmentation.populate(reserve_jobs=True, suppress_errors=True)
+    pupil.Tracking.populate({'tracking_method': 2},
+                            reserve_jobs=True, suppress_errors=True)
+    pupil.FittedPupil.populate(reserve_jobs=True, suppress_errors=True)
+    time.sleep(3600)  # wait an hour before checking again
