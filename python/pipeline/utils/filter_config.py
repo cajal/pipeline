c26d542e42b9a34edccf7fad81afae86c1a134bc
Donnie Kim <kdk411@gmail.com>
2019-08-11 20:07:08 -0500

    comment out

--- python/pipeline/utils/filter_config.py
+++ python/pipeline/utils/filter_config.py
@@ -20,9 +20,9 @@ def key_hash(key):
 class OnlineMedianFilter(dj.Lookup):
     definition = """
     # online median filter parameters
-    filter_hash                         :varchar(32)
+    filter_hash                         : varchar(32)
     ---
-    filter_name="online_median"         : varchar(16)           # filter_name
+    filter_name="online_median"         : varchar(16)           # filter name
     kernel_size                         : tinyint unsigned      # kernel size
     """
 
@@ -37,25 +37,26 @@ class OnlineMedianFilter(dj.Lookup):
             yield k
 
 # @schema
-# class FilterConfig(dj.Lookup):
+# class LowspassFilter(dj.Lookup):
 #     definition = """
-#     # Filter configuration
-#     filter_hash: varchar(32)
+#     # lowpass filter parameters
+#     filter_hash                         : varchar(32)
 #     ---
+#     filter_name="lowpass"               : varchar(16)           # filter name
+#     sampling_freq                       : float                 # sampling freq
+#     cutoff_freq                         : float                 # cutoff freq
 #     """
 
 #     @property
 #     def contents(self):
-#         num_layers = [1]
-#         hidden_state = [8, 16]
-#         input_kernel = [11]
-#         hidden_kernel_size = [1]
-#         regularizer_type = ['LaplaceL2']
-#         regularizer_strength = [0.0, 2.0, 5.0, 10.0, 20.0, 50.0]
+#         filter_name = ["lowpass"]
+#         sampling_freq = [20.0]
+#         cutoff_freq = [5.0, 7.0, 9.0]
 
-#         for p in product(hidden_state, input_kernel, hidden_kernel_size,
-#                          num_layers, regularizer_type, regularizer_strength):
-
-#             k = dict(zip(self.heading.dependent_attributes, p))
-#             k['core_config_hash'] = key_hash(k)
+#         for p in product(filter_name, sampling_freq, cutoff_freq):
+#             k = dict(zip(self.heading.dependent_attributes,p))
+#             k['filter_hash'] = key_hash(k)
 #             yield k
+
+# @schema
+# class FilterTable(dj.)
