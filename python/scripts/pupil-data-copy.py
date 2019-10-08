2369382db0af12c42e0323cdc7eabbb17b6bbd60
Donnie Kim <kdk411@gmail.com>
2019-10-08 15:28:35 -0500

    script to move data from old table to new

---
+++ python/scripts/pupil-data-copy.py
@@ -0,0 +1,11 @@
+from pipeline import pupil
+import datajoint as dj
+
+old_table = pupil.ManuallyTrackedContours.proj()
+already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.Tracking & 'tracking_method=1'))
+to_be_processed = old_table - already_processed
+
+pupil.Tracking.populate(to_be_processed, {'tracking_method':1},
+                        reserve_jobs=True)
+pupil.FittedPupil.populate(to_be_processed, {'tracking_method':1},
+                           reserve_jobs=True)
