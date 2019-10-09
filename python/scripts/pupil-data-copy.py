04677af98759c79fb804f5d36288033366b878bf
Donnie Kim <kdk411@gmail.com>
2019-10-09 14:21:26 -0500

    comment out tracking

--- python/scripts/pupil-data-copy.py
+++ python/scripts/pupil-data-copy.py
@@ -5,7 +5,7 @@ old_table = pupil.ManuallyTrackedContours.proj()
 already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.Tracking & 'tracking_method=1'))
 to_be_processed = old_table - already_processed
 
-pupil.Tracking.populate(to_be_processed, {'tracking_method':1},
-                        reserve_jobs=True, suppress_errors=True)
+# pupil.Tracking.populate(to_be_processed, {'tracking_method':1},
+#                         reserve_jobs=True, suppress_errors=True)
 pupil.FittedPupil.populate(to_be_processed, {'tracking_method':1},
                            reserve_jobs=True, suppress_errors=True)
