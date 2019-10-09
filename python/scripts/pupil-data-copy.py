660ff65f4eea815f41995e2f9c32d541586201f7
Donnie Kim <kdk411@gmail.com>
2019-10-09 14:31:05 -0500

    only run fitting process

--- python/scripts/pupil-data-copy.py
+++ python/scripts/pupil-data-copy.py
@@ -1,11 +1,16 @@
 from pipeline import pupil
 import datajoint as dj
 
-old_table = pupil.ManuallyTrackedContours.proj()
-already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.Tracking & 'tracking_method=1'))
-to_be_processed = old_table - already_processed
+# Tracking
 
-# pupil.Tracking.populate(to_be_processed, {'tracking_method':1},
-#                         reserve_jobs=True, suppress_errors=True)
-pupil.FittedPupil.populate(to_be_processed, {'tracking_method':1},
-                           reserve_jobs=True, suppress_errors=True)
+# tracking_old_table = pupil.ManuallyTrackedContours.proj()
+# tracking_already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.Tracking & 'tracking_method=1'))
+# tracking_to_be_processed = tracking_old_table - tracking_already_processed
+
+# Fitting
+Fitting_old_table = pupil.FittedContour.proj()
+Fitting_already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.FittedPupil & 'tracking_method=1'))
+Fitting_to_be_processed = Fitting_old_table - Fitting_already_processed
+
+pupil.FittedPupil.populate(Fitting_to_be_processed, {'tracking_method':1},
+                        reserve_jobs=True, suppress_errors=True)
