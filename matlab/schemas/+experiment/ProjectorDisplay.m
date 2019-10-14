dc76e471f79397e4ddc7b9a22ae57498b526cf59
Donnie Kim <kdk411@gmail.com>
2019-10-14 17:19:58 -0500

    add Projector and ProjectorDisplay tables for MATLAB

---
+++ matlab/schemas/+experiment/ProjectorDisplay.m
@@ -0,0 +1,12 @@
+%{
+experiment.ProjectorDisplay (Manual) # 
+-> Projector
+-> Rig
+---
+displayed_width     : float     # width in cm
+displayed_height    : float     # height in cm
+%}
+
+
+classdef ProjectorDisplay < dj.Manual
+end
