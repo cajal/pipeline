ac2e4906ecd1058b45620c48b8868647315585b4
Donnie Kim <kdk411@gmail.com>
2019-10-18 12:18:10 -0500

    complete fitting process

--- python/scripts/pupil-data-copy.py
+++ python/scripts/pupil-data-copy.py
@@ -4,7 +4,7 @@ import datajoint as dj
 # Tracking
 
 tracking_old_table = pupil.ManuallyTrackedContours.proj()
-tracking_already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.Tracking & 'tracking_method=1'))
+tracking_already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.FittedPupil & 'tracking_method=1'))
 tracking_to_be_processed = tracking_old_table - tracking_already_processed
 
 # Fitting
@@ -12,8 +12,8 @@ tracking_to_be_processed = tracking_old_table - tracking_already_processed
 # Fitting_already_processed = (dj.U('animal_id', 'session', 'scan_idx') & (pupil.FittedPupil & 'tracking_method=1'))
 # Fitting_to_be_processed = Fitting_old_table - Fitting_already_processed
 
-pupil.Tracking.populate(tracking_to_be_processed, {'tracking_method':1},
-                        reserve_jobs=True, suppress_errors=True)
+# pupil.Tracking.populate(tracking_to_be_processed, {'tracking_method':1},
+#                         reserve_jobs=True, suppress_errors=True)
 pupil.FittedPupil.populate(tracking_to_be_processed, {'tracking_method':1},
                         reserve_jobs=True, suppress_errors=True)
 
