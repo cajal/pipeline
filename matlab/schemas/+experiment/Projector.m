6d0c5432fae1c301772eabed9a1957ec78ca46a8
Donnie Kim <kdk411@gmail.com>
2019-10-29 15:46:56 -0500

    add blue as an option

--- matlab/schemas/+experiment/Projector.m
+++ matlab/schemas/+experiment/Projector.m
@@ -1,13 +1,13 @@
 %{
 # projector specifications
-projector_id        : tinyint                       # projector id
+projector_id        : tinyint                               # projector id
 ---
-pixel_width         : smallint                      # number of pixels in width
-pixel_height        : smallint                      # number of pixels in height
-red                 : enum('UV', 'Green', 'None')   # color to be used for red channel
-green               : enum('UV', 'Green', 'None')   # color to be used for green channel
-blue                : enum('UV', 'Green', 'None')   # color to be used for blue channel
-refresh_rate        : tinyint                       # refresh rate in Hz
+pixel_width         : smallint                              # number of pixels in width
+pixel_height        : smallint                              # number of pixels in height
+red                 : enum('UV', 'Green', 'Blue', 'None')   # color to be used for red channel
+green               : enum('UV', 'Green', 'Blue', 'None')   # color to be used for green channel
+blue                : enum('UV', 'Green', 'Blue', 'None')   # color to be used for blue channel
+refresh_rate        : tinyint                               # refresh rate in Hz
 %}
 
 classdef Projector < dj.Lookup
