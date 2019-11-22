63a8c01d21c0b59bd61cdbdb06b2801093205611
Donnie Kim <kdk411@gmail.com>
2019-11-22 16:50:56 -0600

    update to match with python version

--- matlab/schemas/+experiment/ProjectorColor.m
+++ matlab/schemas/+experiment/ProjectorColor.m
@@ -1,8 +1,7 @@
 %{
 # color options for projector channels
-color_id            : tinyint                   # color id
----
 color               : varchar(32)               # color name
+---
 %}
 
 classdef ProjectorColor < dj.Lookup
