5abdabde134b7f2ada162513fe7eeac067289865
Donnie Kim <kdk411@gmail.com>
2019-11-22 16:48:22 -0600

    delete unnecessary m file

--- python/pipeline/ProjectorColor.m
+++
@@ -1,9 +0,0 @@
-%{
-# color options for projector channels
-color_id            : tinyint                   # color id
----
-color               : varchar(32)               # color name
-%}
-
-classdef Projector < dj.Lookup
-end
