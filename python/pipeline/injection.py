b8dbee9bfc294e5d898d82ba47a83fa4fe140715
Jacob Reimer <jreimer@cns.bcm.edu>
2020-09-23 05:56:38 -0400

    Update injection.py

--- python/pipeline/injection.py
+++ python/pipeline/injection.py
@@ -27,7 +27,7 @@ class InjectionSession(dj.Manual):
     definition = """
     # Injection Session
     -> mice.Mice
-    injection_session=1           : tinyint unsigned                    # injection session order, differ only by AP and ML, not DV
+    injection_session=1           : tinyint unsigned                    # injection session index
     injection_datetime            : datetime                            # time of injection 'YYYY-MM-DD HH:MM:SS' 
     ---
     injection_session_notes=''    : varchar(255)                        # notes
@@ -37,20 +37,19 @@ class InjectionSession(dj.Manual):
 @schema
 class InjectionSite(dj.Manual):
     definition = """
-    # Injection sites information, all injections are based on intrinsic imaging results 
+    # Injection site information
     ->InjectionSession
-    injection_site=1              : tinyint unsigned                    # injection sites order
+    injection_site=1              : tinyint unsigned                    # injection site index
     ---
     -> [nullable] Target
     coord_ant_post                : decimal(3,2)                        # (mm) anterior posterior coordinate from transverse vein
     coord_med_lat                 : decimal(3,2)                        # (mm) medial lateral coordinate from midline
     coord_depth                   : decimal(3,2)                        # (mm) depth from brain surface
-    coord_depth_initial           : decimal(3,2)                        # (mm) the furtherest depth the syringe was lowered at initially to create space for injection
-    tip_size                      : tinyint unsigned                    # (um), 40um-60um
+    coord_depth_initial           : decimal(3,2)                        # (mm) the deepest initial penetration of the pipette
+    tip_size                      : tinyint unsigned                    # (um) 
     -> [nullable] virus.Virus
     virus_total                   : tinyint unsigned                    # (nL) the total virus volume for each injection site
-    virus_withdraw_speed=10       : tinyint unsigned                    # (nL/s) virus withdraw speed for syringe by nano pump
-    virus_injection_speed=4       : tinyint unsigned                    # (nL/s) virus injection speed for syringe by nano pump
-    visualize_level_drop          : bool                                # visualize whether or not virus level drop inside tip
+    virus_injection_speed=4       : tinyint unsigned                    # (nL/s) speed of injection with nano pump
+    visualize_confirmation        : bool                                # Was virus extrusion visually confirmed?
     injection_site_notes=''       : varchar(255)                        # notes
     """
