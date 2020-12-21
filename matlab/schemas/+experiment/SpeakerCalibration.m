77f047ccefa33775f7c4df513f1b2fbb8097cf1a
Saumil Patel <spatel@bcm.edu>
2020-12-20 21:04:01 -0600

    audio support

---
+++ matlab/schemas/+experiment/SpeakerCalibration.m
@@ -0,0 +1,17 @@
+%{
+# speaker sound pressure calibration
+speaker_id              : int               # some number that identifies the speaker
+trial                   : int               # trial number of calibration
+---
+sampling_freq           : float             # sampling frequency used during calibration
+test_voltage            : float             # test voltage in volts used to drive the audio input which via amp drives the speaker
+desired_spl             : float             # desired sound pressure after equalization in dB
+frequencies             : mediumblob        # frequencies presented during calibration
+response                : mediumblob        # compensatory response to equalize all presented frequencies to a fixed output level when speaker isdriven by 1V
+fir_coefficients        : mediumblob        # coefficients of equalizing fir filter
+ts                      : timestamp         # timestamp
+%}
+
+classdef SpeakerCalibration < dj.Manual
+end
+
