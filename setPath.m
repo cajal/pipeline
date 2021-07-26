3e51cad83b56cb0f8282cf146d2b4a1ad061522a
Saumil Patel <spatel@bcm.edu>
2021-07-26 16:19:26 -0500

    added code to support a list of speakers currently used on all setups

---
+++ setPath.m
@@ -0,0 +1,10 @@
+addpath c:\pipeline
+addpath c:\pipeline\datajoint
+addpath(genpath('c:\pipeline\commons\schemas'));
+addpath('c:\pipeline\commons\lib');
+addpath('c:\pipeline\pipeline\matlab\');
+addpath('c:\pipeline\pipeline\matlab\schemas');
+addpath('c:\pipeline\pipeline\matlab\lib');
+addpath('c:\cajal\auditory_stimuli\matlab') ;
+addpath('c:\cajal\auditory_stimuli\matlab\conf') ;
+run c:\pipeline\mym\mymSetup;
