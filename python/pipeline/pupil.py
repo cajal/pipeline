8ccc0dc9572e1891401d9d2a6d3703ce0d50e901
Tony Ramos <anthonyjoeramos@gmail.com>
2021-09-11 19:17:26 -0500

    changed None to np.nan

--- python/pipeline/pupil.py
+++ python/pipeline/pupil.py
@@ -962,7 +962,7 @@ class FittedPupil(dj.Computed):
         rejected_ind = DLC_tools.filter_by_fitting_std(
             data=data_circle, fitting_method='circle', std_magnitude=5.5)
 
-        data_circle[rejected_ind] = None, None, -3.0
+        data_circle[rejected_ind] = np.nan, np.nan, -3.0
 
         common_entry = np.array(list(key.values()))
         common_matrix = np.tile(common_entry, (nframes, 1))
@@ -971,19 +971,19 @@ class FittedPupil(dj.Computed):
             (common_matrix, np.arange(nframes).reshape(-1, 1), data_circle))
 
         # insert data
-        data_circle = data_circle.astype(np.float)
+        
         self.Circle.insert(data_circle)
 
         # now repeat the process for ellipse
         rejected_ind = DLC_tools.filter_by_fitting_std(
             data=data_ellipse, fitting_method='ellipse', std_magnitude=5.5)
 
-        data_ellipse[rejected_ind, :] = None, None, None, None, -3.0
+        data_ellipse[rejected_ind, :] = np.nan, np.nan, np.nan, np.nan, -3.0
 
         data_ellipse = np.hstack(
             (common_matrix, np.arange(nframes).reshape(-1, 1), data_ellipse))
 
-        data_ellipse = data_ellipse.astype(np.float)
+
         self.Ellipse.insert(data_ellipse)
 
 
