f8dc050b32be6c8f752f11b57eb4088f9bfba524
Tony Ramos <anthonyjoeramos@gmail.com>
2021-09-15 15:54:21 -0500

    added fitted pupil and fitted_pupil cronjob using datajoint 0.11 container

---
+++ python/scripts/fitted_pupil.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+from pipeline import experiment, reso, meso, fuse, stack, pupil, treadmill, posture
+from stimulus import stimulus
+from stimline import tune
+import time
+import logging
+import datajoint as dj
+
+## database logging code 
+
+logging.basicConfig(level=logging.ERROR)
+logging.getLogger('datajoint.connection').setLevel(logging.DEBUG)
+if hasattr(dj.connection, 'query_log_max_length'):
+    dj.connection.query_log_max_length = 3000 
+
+
+# # Scans
+# for priority in range(120, -130, -10):  # highest to lowest priority
+#     next_scans = (experiment.AutoProcessing() & 'priority > {}'.format(priority) &
+#                   (experiment.Scan() & 'scan_ts > "2019-01-01 00:00:00"'))
+
+next_scans = (experiment.AutoProcessing  & 'priority < 120' &
+              (experiment.Scan & 'scan_ts > "2019-01-01 00:00:00"'))
+
+pupil.FittedPupil().populate(next_scans)
