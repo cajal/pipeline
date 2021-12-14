bd6b597f9e8532f94ef1311078048ee8170c5e01
Anthony Ramos <anthonyjoeramos@gmail.com>
2021-12-14 09:44:31 -0600

    added kubenernetes code to add taint

--- python/scripts/manager-mcl.py
+++ python/scripts/manager-mcl.py
@@ -1,7 +1,11 @@
 #!/usr/local/bin/python3
-import kubernetes
+
+from kubernetes import client,config
 import time
 import datajoint as dj
+
+config.load_incluster_config()
+v1 = client.CoreV1Api()
 
 experiment = dj.create_virtual_module('experiment', 'pipeline_experiment')
 treadmill = dj.create_virtual_module('treadmill', 'pipeline_treadmill')
@@ -14,25 +18,28 @@ while True:
     unfinished_treadmill = experiment.MesoClosedLoop - treadmill.Treadmill
     unfinished_pupil = experiment.MesoClosedLoop - pupil.Eye
     unfinished_fuse = experiment.MesoClosedLoop - fuse.ScanDone
-
+    nodes = ['at-compute003','at-compute004','at-compute005']
+    nodes = v1.list_node(label_selector=f'kubernetes.io/hostname in ({','.join(nodes)})')
+        
     if unfinished_treadmill or unfinished_pupil or unfinished_fuse:
-        pass
-
-        '''
-        TODO: add meso_closed_loop=true:NoExecute taint to the following nodes:
-            at-compute003
-            at-compute004
-            at-compute005
-        '''
+        for node in nodes.items:
+            body = {
+                'spec': {
+                    'taints':[{
+                        'key':'meso_closed_loop',
+                        'operator':'Equal',
+                        'value':'true',
+                        'effect':'NoExecute'
+                    }]
+                }
+            }
+            v1.patch_node(node.metadata.name,body)
         
     else:
-        pass
+        for node in nodes.items:
 
-        '''
-        TODO: remove meso_closed_loop=true:NoExecute taint from the following nodes:
-            at-compute003
-            at-compute004
-            at-compute005
-        '''
+            if node.spec.taints is not None:
+                node.spec.taints = [taint for taint in node.spec.taints if not taint.key == 'meso_closed_loop']
+            v1.patch_node(node.metadata.name,node)
 
     time.sleep(60)
